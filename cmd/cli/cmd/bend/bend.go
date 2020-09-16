5e29e7e4ff431c6f811ef5774072a7e0d35e2984
richard <richard.mace@gmail.com>
2020-09-16 16:56:48 +0100

    tagged for version 0.32.2

--- cmd/cli/cmd/bend/bend.go
+++ cmd/cli/cmd/bend/bend.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	CAppName        string = "BoxWallet"
-	CBWAppVersion   string = "0.32.1"
+	CBWAppVersion   string = "0.32.2"
 	CAppFilename    string = "boxwallet"
 	CAppFilenameWin string = "boxwallet.exe"
 	CAppLogfile     string = "boxwallet.log"
