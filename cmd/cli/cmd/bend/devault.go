feaaa761302796790f5e67dcfc12e5486e4713d9
richard <richard.mace@gmail.com>
2020-11-24 14:00:31 +0000

    Added more DeVault functionality.

--- cmd/cli/cmd/bend/devault.go
+++ cmd/cli/cmd/bend/devault.go
@@ -1,10 +1,13 @@
 package bend
 
 import (
+	"bytes"
 	"encoding/json"
+	"github.com/dustin/go-humanize"
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 const (
@@ -34,27 +37,215 @@ const (
 	CDeVaultRPCPort string = "3339"
 )
 
-// Below may need updating as haven't run it live
-type DVTWalletInfoRespStruct struct {
+type DVTBlockchainInfoRespStruct struct {
 	Result struct {
-		Walletname            string  `json:"walletname"`
-		Walletversion         int     `json:"walletversion"`
-		Balance               float64 `json:"balance"`
-		UnconfirmedBalance    float64 `json:"unconfirmed_balance"`
-		ImmatureBalance       float64 `json:"immature_balance"`
-		Txcount               int     `json:"txcount"`
-		Keypoololdest         int     `json:"keypoololdest"`
-		Keypoolsize           int     `json:"keypoolsize"`
-		Hdseedid              string  `json:"hdseedid"`
-		KeypoolsizeHdInternal int     `json:"keypoolsize_hd_internal"`
-		Paytxfee              float64 `json:"paytxfee"`
-		PrivateKeysEnabled    bool    `json:"private_keys_enabled"`
-		AvoidReuse            bool    `json:"avoid_reuse"`
-		Scanning              bool    `json:"scanning"`
-		UnlockedUntil         int     `json:"unlocked_until"`
+		Chain                string  `json:"chain"`
+		Blocks               int     `json:"blocks"`
+		Headers              int     `json:"headers"`
+		Bestblockhash        string  `json:"bestblockhash"`
+		Difficulty           float64 `json:"difficulty"`
+		Mediantime           int     `json:"mediantime"`
+		Verificationprogress float64 `json:"verificationprogress"`
+		Initialblockdownload bool    `json:"initialblockdownload"`
+		Chainwork            string  `json:"chainwork"`
+		Coinsupply           float64 `json:"coinsupply"`
+		Pruned               bool    `json:"pruned"`
+		Warnings             string  `json:"warnings"`
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
+}
+
+type DVTListReceivedByAddressRespStruct struct {
+	Result []struct {
+		Address       string        `json:"address"`
+		Amount        float64       `json:"amount"`
+		Confirmations int           `json:"confirmations"`
+		Label         string        `json:"label"`
+		Txids         []interface{} `json:"txids"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type DVTNetworkInfoRespStruct struct {
+	Result struct {
+		Version         int    `json:"version"`
+		Subversion      string `json:"subversion"`
+		Protocolversion int    `json:"protocolversion"`
+		Localservices   string `json:"localservices"`
+		Localrelay      bool   `json:"localrelay"`
+		Timeoffset      int    `json:"timeoffset"`
+		Networkactive   bool   `json:"networkactive"`
+		Connections     int    `json:"connections"`
+		Networks        []struct {
+			Name                      string `json:"name"`
+			Limited                   bool   `json:"limited"`
+			Reachable                 bool   `json:"reachable"`
+			Proxy                     string `json:"proxy"`
+			ProxyRandomizeCredentials bool   `json:"proxy_randomize_credentials"`
+		} `json:"networks"`
+		Relayfee         float64       `json:"relayfee"`
+		Excessutxocharge float64       `json:"excessutxocharge"`
+		Localaddresses   []interface{} `json:"localaddresses"`
+		Warnings         string        `json:"warnings"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type DVTWalletInfoRespStruct struct {
+	Result struct {
+		Walletname         string  `json:"walletname"`
+		Walletversion      int     `json:"walletversion"`
+		Balance            float64 `json:"balance"`
+		UnconfirmedBalance float64 `json:"unconfirmed_balance"`
+		ImmatureBalance    float64 `json:"immature_balance"`
+		Txcount            int     `json:"txcount"`
+		Keypoololdest      int     `json:"keypoololdest"`
+		Keypoolsize        int     `json:"keypoolsize"`
+		Hdchainid          string  `json:"hdchainid"`
+		Hdaccountcount     int     `json:"hdaccountcount"`
+		Hdaccounts         []struct {
+			Hdaccountindex     int `json:"hdaccountindex"`
+			Hdexternalkeyindex int `json:"hdexternalkeyindex"`
+			Hdinternalkeyindex int `json:"hdinternalkeyindex"`
+		} `json:"hdaccounts"`
+		UnlockedUntil      int     `json:"unlocked_until"`
+		Paytxfee           float64 `json:"paytxfee"`
+		PrivateKeysEnabled bool    `json:"private_keys_enabled"`
+		HasBlsKeys         bool    `json:"has bls keys"`
+		HasLegacyKeys      bool    `json:"has legacy keys"`
+		IsBlank            bool    `json:"is blank"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+func GetBlockchainInfoDVT(cliConf *ConfStruct) (DVTBlockchainInfoRespStruct, error) {
+	var respStruct DVTBlockchainInfoRespStruct
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getblockchaininfo\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func GetBlockchainSyncTxtDVT(synced bool, bci *DVTBlockchainInfoRespStruct) string {
+	s := ConvertBCVerification(bci.Result.Verificationprogress)
+	if s == "0.0" {
+		s = ""
+	} else {
+		s = s + "%"
+	}
+
+	if !synced {
+		if bci.Result.Verificationprogress > gLastBCSyncPos {
+			gLastBCSyncPos = bci.Result.Verificationprogress
+			return "Blockchain:  [syncing " + s + " ](fg:yellow)"
+		} else {
+			gLastBCSyncPos = bci.Result.Verificationprogress
+			return "Blockchain:  [waiting " + s + " ](fg:yellow)"
+		}
+	} else {
+		return "Blockchain:  [synced " + CUtfTickBold + "](fg:green)"
+	}
+}
+
+func GetNetworkBlocksTxtDVT(bci *DVTBlockchainInfoRespStruct) string {
+	blocksStr := humanize.Comma(int64(bci.Result.Blocks))
+
+	if blocksStr == "0" {
+		return "Blocks:      [waiting...](fg:white)"
+	}
+
+	return "Blocks:      [" + blocksStr + "](fg:green)"
+
+}
+
+func GetNetworkDifficultyTxtDVT(difficulty, good, warn float64) string {
+	var s string
+	if difficulty > 1000 {
+		s = humanize.FormatFloat("#.#", difficulty/1000) + "k"
+	} else {
+		s = humanize.Ftoa(difficulty)
+	}
+
+	// If Diff is less than 1, then we're not even calculating it properly yet...
+	if difficulty < 1 {
+		return "[Difficulty:  waiting...](fg:white)"
+	}
+
+	if difficulty >= good {
+		return "Difficulty:  [" + s + "](fg:green)"
+	} else if difficulty >= warn {
+		return "Difficulty:  [" + s + "](fg:yellow)"
+	} else {
+		return "Difficulty:  [" + s + "](fg:red)"
+	}
+}
+
+func GetNetworkHeadersTxtDVT(bci *DVTBlockchainInfoRespStruct) string {
+	headersStr := humanize.Comma(int64(bci.Result.Headers))
+
+	if bci.Result.Headers > 1 {
+		return "Headers:     [" + headersStr + "](fg:green)"
+	} else {
+		return "[Headers:     " + headersStr + "](fg:red)"
+	}
+}
+
+func GetNetworkInfoDVT(cliConf *ConfStruct) (DVTNetworkInfoRespStruct, error) {
+	var respStruct DVTNetworkInfoRespStruct
+
+	for i := 1; i < 50; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getnetworkinfo\",\"params\":[]}")
+
+		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+		if err != nil {
+			return respStruct, err
+		}
+		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return respStruct, err
+		}
+		defer resp.Body.Close()
+		bodyResp, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return respStruct, err
+		}
+
+		// Check to make sure we are not loading the wallet
+		if bytes.Contains(bodyResp, []byte("Loading")) {
+			// The wallet is still loading, so print message, and sleep for 3 seconds and try again...
+			time.Sleep(5 * time.Second)
+		} else {
+			_ = json.Unmarshal(bodyResp, &respStruct)
+			return respStruct, err
+		}
+	}
+	return respStruct, nil
 }
 
 func GetWalletInfoDVT(cliConf *ConfStruct) (DVTWalletInfoRespStruct, error) {
@@ -90,4 +281,78 @@ func GetWalletInfoDVT(cliConf *ConfStruct) (DVTWalletInfoRespStruct, error) {
 	}
 
 	return respStruct, nil
+}
+
+func GetWalletSecurityStateDVT(wi *DVTWalletInfoRespStruct) WEType {
+	if wi.Result.UnlockedUntil == 0 {
+		return WETLocked
+	} else if wi.Result.UnlockedUntil == -1 {
+		return WETUnencrypted
+	} else if wi.Result.UnlockedUntil > 0 {
+		return WETUnlockedForStaking
+	} else {
+		return WETUnknown
+	}
+}
+
+func ListReceivedByAddressDVT(cliConf *ConfStruct, includeZero bool) (DVTListReceivedByAddressRespStruct, error) {
+	var respStruct DVTListReceivedByAddressRespStruct
+
+	var s string
+	if includeZero {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listreceivedbyaddress\",\"params\":[1, true]}"
+	} else {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listreceivedbyaddress\",\"params\":[1, false]}"
+	}
+	body := strings.NewReader(s)
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func UnlockWalleDVT(cliConf *ConfStruct, pw string) error {
+	var respStruct GenericRespStruct
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"walletpassphrase\",\"params\":[\"" + pw + "\",0]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return err
+	}
+	return nil
 }
