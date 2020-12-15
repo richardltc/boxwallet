481644824897c8a292d9a252630150e682e396ae
richard <richard.mace@gmail.com>
2020-12-15 20:19:28 +0000

    Started work on new BoxWallet updater app

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -7,57 +7,67 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	be "richardmace.co.uk/boxwallet/cmd/cli/cmd/bend"
 	"runtime"
 
+	"github.com/mitchellh/go-ps"
 	gwc "github.com/richardltc/gwcommon"
-
 	m "richardmace.co.uk/godivi/pkg/models"
 )
 
+const (
+	cAppName              = "bwupdater"
+	cAppCLIFileWinBoxDivi = "boxwallet.exe"
+	cAppCLIFileBoxDivi    = "boxwallet"
+	cAppVersion           = "0.35.1"
+)
+
 func main() {
-	sAppName, err := gwc.GetAppName(gwc.APPTCLI)
-	if err != nil {
-		log.Fatal("Unable to GetAppName " + err.Error())
-	}
+	//sAppName, err := gwc.GetAppName(gwc.APPTCLI)
+	//if err != nil {
+	//	log.Fatal("Unable to GetAppName " + err.Error())
+	//}
 
-	sAppCLIName, err := gwc.GetAppCLIName(gwc.APPTCLI)
-	if err != nil {
-		log.Fatal("Unable to GetAppCLIName " + err.Error())
-	}
+	//sAppCLIName, err := gwc.GetAppCLIName(gwc.APPTCLI)
+	//if err != nil {
+	//	log.Fatal("Unable to GetAppCLIName " + err.Error())
+	//}
+	//
+	//sAppCLIFilename, err := gwc.GetAppFileName(gwc.APPTCLI) //GetAppCLIFileName()
+	//if err != nil {
+	//	log.Fatal("Unable to GetAppFileName " + err.Error())
+	//}
+	//sAppInstallerFn, err := gwc.GetAppFileName(gwc.APPTInstaller)
+	//if err != nil {
+	//	log.Fatal("Unable to GetAppInstallerFileName " + err.Error())
+	//}
+	//sAppUpdaterName, err := gwc.GetAppFileName(gwc.APPTUpdater)
+	//if err != nil {
+	//	log.Fatal("Unable to GetAppFileName " + err.Error())
+	//}
 
-	sAppCLIFilename, err := gwc.GetAppFileName(gwc.APPTCLI) //GetAppCLIFileName()
-	if err != nil {
-		log.Fatal("Unable to GetAppFileName " + err.Error())
-	}
-	sAppInstallerFn, err := gwc.GetAppFileName(gwc.APPTInstaller)
-	if err != nil {
-		log.Fatal("Unable to GetAppInstallerFileName " + err.Error())
-	}
-	sAppUpdaterName, err := gwc.GetAppFileName(gwc.APPTUpdater)
-	if err != nil {
-		log.Fatal("Unable to GetAppFileName " + err.Error())
-	}
+	fmt.Println(cAppName + " v" + cAppVersion + " Started...")
 
-	fmt.Println(sAppUpdaterName + " v" + gwc.CAppVersion + " Started...")
-	// Check to make sure we're installed
-	if !gwc.IsGoWalletInstalled(gwc.APPTCLI) {
-		log.Fatal(sAppName + ` doesn't appear to be installed yet. Please run "` + sAppCLIFilename + ` install" first`)
-	}
-
-	// Check to make sure that the app (GoDivi) is not currently running
-	gdr, _, err := gwc.IsAppCLIRunning()
+	// Check to make sure that the app (BoxWallet) is not currently running
+	gdr, _, err := isBoxWalletRunning()
 	if err != nil {
-		log.Fatal("Unable to determine if " + sAppCLIFilename + " is running - " + err.Error())
+		log.Fatal("Unable to determine if " + cAppCLIFileBoxDivi + " is running - " + err.Error())
 	}
 	if gdr {
-		log.Fatal(sAppCLIFilename + " is currently running.  Please close " + sAppCLIFilename + " and then run " + sAppUpdaterName + " again.")
+		log.Fatal(cAppCLIFileBoxDivi + " is currently running.  Please close " + cAppCLIFileBoxDivi + " and then run " + cAppName + " again.")
 	}
 
-	dbf, _ := gwc.GetAppsBinFolder(gwc.APPTCLI)
-	dir, err := gwc.GetRunningDir()
+	//dbf, _ := gwc.GetAppsBinFolder(gwc.APPTCLI)
+	//dir, err := gwc.GetRunningDir()
+	//if err != nil {
+	//	log.Fatal("Unable to GetRunningDir " + err.Error())
+	//}
+	ex, err := os.Executable()
 	if err != nil {
-		log.Fatal("Unable to GetRunningDir " + err.Error())
+		log.Fatal("unable to retrieve running binary: %v ", err)
 	}
+	dir := be.AddTrailingSlash(filepath.Dir(ex))
 
 	var url, file string
 	// Get download file link
@@ -121,6 +131,24 @@ func main() {
 
 // 	return "http://" + myURL.Host + path
 // }
+
+func isBoxWalletRunning() (bool, int, error) {
+	var pid int
+	var err error
+	if runtime.GOOS == "windows" {
+		pid, _, err = findProcess(cAppCLIFileWinBoxDivi)
+	} else {
+		pid, _, err = findProcess(cAppCLIFileBoxDivi)
+	}
+
+	if err == nil {
+		return true, pid, nil
+	} else if err.Error() == "not found" {
+		return false, 0, nil
+	} else {
+		return false, 0, err
+	}
+}
 
 func tellGDServerToShutdown() gwc.ServerResponse {
 	ss := m.ServerRequestStruct{}
