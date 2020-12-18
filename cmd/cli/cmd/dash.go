a5ef8c5213e38f755bc36782031d37d1eebbd4de
richard <richard.mace@gmail.com>
2020-12-18 20:49:30 +0000

    Added version info to dash command

--- cmd/cli/cmd/dash.go
+++ cmd/cli/cmd/dash.go
@@ -79,7 +79,7 @@ var dashCmd = &cobra.Command{
 	* Wallet encryption status
 	* Balance info`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("  ____          __          __   _ _      _   \n |  _ \\         \\ \\        / /  | | |    | |  \n | |_) | _____  _\\ \\  /\\  / /_ _| | | ___| |_ \n |  _ < / _ \\ \\/ /\\ \\/  \\/ / _` | | |/ _ \\ __|\n | |_) | (_) >  <  \\  /\\  / (_| | | |  __/ |_ \n |____/ \\___/_/\\_\\  \\/  \\/ \\__,_|_|_|\\___|\\__|\n                                              \n                                              ")
+		fmt.Println("  ____          __          __   _ _      _   \n |  _ \\         \\ \\        / /  | | |    | |  \n | |_) | _____  _\\ \\  /\\  / /_ _| | | ___| |_ \n |  _ < / _ \\ \\/ /\\ \\/  \\/ / _` | | |/ _ \\ __|\n | |_) | (_) >  <  \\  /\\  / (_| | | |  __/ |_ \n |____/ \\___/_/\\_\\  \\/  \\/ \\__,_|_|_|\\___|\\__| v" + be.CBWAppVersion + "\n                                              \n                                               ")
 		// Lets load our config file first, to see if the user has made their coin choice...
 		cliConf, err := be.GetConfigStruct("", true)
 		if err != nil {
