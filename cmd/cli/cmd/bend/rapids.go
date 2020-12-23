e81f6084eb0da22058239b23b992e75d1b207722
richard <richard.mace@gmail.com>
2020-12-23 09:06:34 +0000

    Added core structs for Rapids

--- cmd/cli/cmd/bend/rapids.go
+++ cmd/cli/cmd/bend/rapids.go
@@ -1,5 +1,12 @@
 package bend
 
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
 const (
 	CCoinNameRapids string = "Rapids"
 
@@ -31,6 +38,187 @@ const (
 	CRapidsRPCPort string = "28732"
 )
 
+type RapidsBlockchainInfoRespStruct struct {
+	Result struct {
+		Chain                string  `json:"chain"`
+		Blocks               int     `json:"blocks"`
+		Headers              int     `json:"headers"`
+		Bestblockhash        string  `json:"bestblockhash"`
+		Difficulty           float64 `json:"difficulty"`
+		Verificationprogress float64 `json:"verificationprogress"`
+		Chainwork            string  `json:"chainwork"`
+		Softforks            []struct {
+			ID      string `json:"id"`
+			Version int    `json:"version"`
+			Reject  struct {
+				Status bool `json:"status"`
+			} `json:"reject"`
+		} `json:"softforks"`
+		Upgrades struct {
+			PoS struct {
+				Activationheight int    `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"PoS"`
+			PoSV2 struct {
+				Activationheight int    `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"PoS v2"`
+			Zerocoin struct {
+				Activationheight int64  `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"Zerocoin"`
+			ZerocoinV2 struct {
+				Activationheight int64  `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"Zerocoin v2"`
+			BIP65 struct {
+				Activationheight int    `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"BIP65"`
+			ZerocoinPublic struct {
+				Activationheight int64  `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"Zerocoin Public"`
+			PIVXV34 struct {
+				Activationheight int    `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"PIVX v3.4"`
+			PIVXV41 struct {
+				Activationheight int    `json:"activationheight"`
+				Status           string `json:"status"`
+				Info             string `json:"info"`
+			} `json:"PIVX v4.1"`
+		} `json:"upgrades"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type RapidsGetInfoRespStruct struct {
+	Result struct {
+		Version         int     `json:"version"`
+		Protocolversion int     `json:"protocolversion"`
+		Services        string  `json:"services"`
+		Walletversion   int     `json:"walletversion"`
+		Balance         float64 `json:"balance"`
+		Zerocoinbalance float64 `json:"zerocoinbalance"`
+		StakingStatus   string  `json:"staking status"`
+		Blocks          int     `json:"blocks"`
+		Timeoffset      int     `json:"timeoffset"`
+		Connections     int     `json:"connections"`
+		Proxy           string  `json:"proxy"`
+		Difficulty      float64 `json:"difficulty"`
+		Testnet         bool    `json:"testnet"`
+		Moneysupply     float64 `json:"moneysupply"`
+		ZPIVsupply      struct {
+			Num1    float64 `json:"1"`
+			Num5    float64 `json:"5"`
+			Num10   float64 `json:"10"`
+			Num50   float64 `json:"50"`
+			Num100  float64 `json:"100"`
+			Num500  float64 `json:"500"`
+			Num1000 float64 `json:"1000"`
+			Num5000 float64 `json:"5000"`
+			Total   float64 `json:"total"`
+		} `json:"zPIVsupply"`
+		Keypoololdest int     `json:"keypoololdest"`
+		Keypoolsize   int     `json:"keypoolsize"`
+		Paytxfee      float64 `json:"paytxfee"`
+		Relayfee      float64 `json:"relayfee"`
+		Errors        string  `json:"errors"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type RapidsListReceivedByAddressRespStruct struct {
+	Result []struct {
+		Address         string        `json:"address"`
+		Account         string        `json:"account"`
+		Amount          float64       `json:"amount"`
+		Confirmations   int           `json:"confirmations"`
+		Bcconfirmations int           `json:"bcconfirmations"`
+		Label           string        `json:"label"`
+		Txids           []interface{} `json:"txids"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type RapidsMNSyncStatusRespStruct struct {
+	Result struct {
+		IsBlockchainSynced         bool `json:"IsBlockchainSynced"`
+		LastMasternodeList         int  `json:"lastMasternodeList"`
+		LastMasternodeWinner       int  `json:"lastMasternodeWinner"`
+		LastBudgetItem             int  `json:"lastBudgetItem"`
+		LastFailure                int  `json:"lastFailure"`
+		NCountFailures             int  `json:"nCountFailures"`
+		SumMasternodeList          int  `json:"sumMasternodeList"`
+		SumMasternodeWinner        int  `json:"sumMasternodeWinner"`
+		SumBudgetItemProp          int  `json:"sumBudgetItemProp"`
+		SumBudgetItemFin           int  `json:"sumBudgetItemFin"`
+		CountMasternodeList        int  `json:"countMasternodeList"`
+		CountMasternodeWinner      int  `json:"countMasternodeWinner"`
+		CountBudgetItemProp        int  `json:"countBudgetItemProp"`
+		CountBudgetItemFin         int  `json:"countBudgetItemFin"`
+		RequestedMasternodeAssets  int  `json:"RequestedMasternodeAssets"`
+		RequestedMasternodeAttempt int  `json:"RequestedMasternodeAttempt"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type RapidsStakingStatusRespStruct struct {
+	Result struct {
+		StakingStatus       bool    `json:"staking_status"`
+		StakingEnabled      bool    `json:"staking_enabled"`
+		ColdstakingEnabled  bool    `json:"coldstaking_enabled"`
+		Haveconnections     bool    `json:"haveconnections"`
+		Mnsync              bool    `json:"mnsync"`
+		Walletunlocked      bool    `json:"walletunlocked"`
+		Stakeablecoins      int     `json:"stakeablecoins"`
+		Stakingbalance      float64 `json:"stakingbalance"`
+		Stakesplitthreshold float64 `json:"stakesplitthreshold"`
+		LastattemptAge      int     `json:"lastattempt_age"`
+		LastattemptDepth    int     `json:"lastattempt_depth"`
+		LastattemptHash     string  `json:"lastattempt_hash"`
+		LastattemptCoins    int     `json:"lastattempt_coins"`
+		LastattemptTries    int     `json:"lastattempt_tries"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type RapidsWalletInfoRespStruct struct {
+	Result struct {
+		Walletversion              int     `json:"walletversion"`
+		Balance                    float64 `json:"balance"`
+		DelegatedBalance           float64 `json:"delegated_balance"`
+		ColdStakingBalance         float64 `json:"cold_staking_balance"`
+		UnconfirmedBalance         float64 `json:"unconfirmed_balance"`
+		ImmatureBalance            float64 `json:"immature_balance"`
+		ImmatureDelegatedBalance   float64 `json:"immature_delegated_balance"`
+		ImmatureColdStakingBalance float64 `json:"immature_cold_staking_balance"`
+		Txcount                    int     `json:"txcount"`
+		UnlockedUntil              int     `json:"unlocked_until"`
+		Keypoololdest              int     `json:"keypoololdest"`
+		Keypoolsize                int     `json:"keypoolsize"`
+		Hdseedid                   string  `json:"hdseedid"`
+		KeypoolsizeHdInternal      int     `json:"keypoolsize_hd_internal"`
+		KeypoolsizeHdStaking       int     `json:"keypoolsize_hd_staking"`
+		Paytxfee                   float64 `json:"paytxfee"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
 func getRapidsAddNodes() ([]byte, error) {
 	addnodes := []byte("addnode=104.248.62.138:28732\n" +
 		"addnode=108.61.189.250:58678\n" +
@@ -44,4 +232,85 @@ func getRapidsAddNodes() ([]byte, error) {
 		"addnode=165.22.104.43:46592")
 
 	return addnodes, nil
+}
+
+func GetWalletInfoRapids(cliConf *ConfStruct) (RapidsWalletInfoRespStruct, error) {
+	var respStruct RapidsWalletInfoRespStruct
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getwalletinfo\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	// Check to see if the json response contains "unlocked_until"
+	s := string([]byte(bodyResp))
+	if !strings.Contains(s, "unlocked_until") {
+		respStruct.Result.UnlockedUntil = -1
+	}
+
+	return respStruct, nil
+}
+
+func GetWalletSecurityStateRapids(wi *RapidsWalletInfoRespStruct) WEType {
+	if wi.Result.UnlockedUntil == 0 {
+		return WETLocked
+	} else if wi.Result.UnlockedUntil == -1 {
+		return WETUnencrypted
+	} else if wi.Result.UnlockedUntil > 0 {
+		return WETUnlockedForStaking
+	} else {
+		return WETUnknown
+	}
+}
+
+func ListReceivedByAddressRapids(cliConf *ConfStruct, includeZero bool) (RapidsListReceivedByAddressRespStruct, error) {
+	var respStruct RapidsListReceivedByAddressRespStruct
+
+	var s string
+	if includeZero {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listreceivedbyaddress\",\"params\":[1, true]}"
+	} else {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listreceivedbyaddress\",\"params\":[1, false]}"
+	}
+	body := strings.NewReader(s)
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
 }
