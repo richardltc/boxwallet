e0dd2d4e870f8e5651e7a09205c43c213b591f3e
richard <richard.mace@gmail.com>
2021-01-14 14:00:48 +0000

    Fixed tidying up for downloaded file for Phore on Windows

--- cmd/cli/cmd/coin.go
+++ cmd/cli/cmd/coin.go
@@ -443,11 +443,11 @@ func doRequiredFiles() error {
 		}
 	case be.PTPhore:
 		if runtime.GOOS == "windows" {
-			_, err = be.UnZip(filePath, "tmp")
+			_, err = be.UnZip(filePath, abf)
 			if err != nil {
 				return fmt.Errorf("unable to unzip file: %v - %v", filePath, err)
 			}
-			defer os.RemoveAll("tmp")
+			defer os.RemoveAll(abf)
 		} else if runtime.GOARCH == "arm" {
 			//err = be.ExtractTarGz(r)
 			err = archiver.Unarchive(filePath, abf)
