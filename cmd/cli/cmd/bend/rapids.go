228cc1183610f4cbf4672bf5042e64a46730f6de
richard <richard.mace@gmail.com>
2021-01-16 07:41:15 +0000

    Added checking for "Verifying" message when waiting for wallet to load, so we don't return an error for Rapids

--- cmd/cli/cmd/bend/rapids.go
+++ cmd/cli/cmd/bend/rapids.go
@@ -292,7 +292,9 @@ func GetInfoRapids(cliConf *ConfStruct) (RapidsGetInfoRespStruct, error) {
 		}
 
 		// Check to make sure we are not loading the wallet
-		if bytes.Contains(bodyResp, []byte("Loading")) || bytes.Contains(bodyResp, []byte("Rewinding")) {
+		if bytes.Contains(bodyResp, []byte("Loading")) ||
+			bytes.Contains(bodyResp, []byte("Rewinding")) ||
+			bytes.Contains(bodyResp, []byte("Verifying")) {
 			// The wallet is still loading, so print message, and sleep for 3 seconds and try again..
 			var errStruct GenericRespStruct
 			err = json.Unmarshal(bodyResp, &errStruct)
