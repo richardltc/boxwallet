a68e5c9f32ef3b762f1ef0237d605f89147fa138
richard <richard.mace@gmail.com>
2021-01-19 14:03:11 +0000

    Added extra feedback info when waiting for DIVI wallet to load

--- cmd/cli/cmd/bend/divi.go
+++ cmd/cli/cmd/bend/divi.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/dustin/go-humanize"
+	"github.com/theckman/yacspin"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -366,6 +367,65 @@ func GetInfoDivi(cliConf *ConfStruct) (diviGetInfoRespStruct, error) {
 		}
 	}
 	return respStruct, nil
+}
+
+func GetInfoDIVIUI(cliConf *ConfStruct, spin *yacspin.Spinner) (diviGetInfoRespStruct, string, error) {
+	var respStruct diviGetInfoRespStruct
+
+	for i := 1; i < 600; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetInfo + "\",\"params\":[]}")
+		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+		if err != nil {
+			return respStruct, "", err
+		}
+		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		for j := 1; j < 60; j++ {
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				spin.Message(" waiting for your " + CCoinNamePIVX + " wallet to respond, this could take several minutes...")
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			defer resp.Body.Close()
+			bodyResp, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return respStruct, "", err
+			}
+
+			// Check to make sure we are not loading the wallet
+			if bytes.Contains(bodyResp, []byte("Loading")) ||
+				bytes.Contains(bodyResp, []byte("Rescanning")) ||
+				bytes.Contains(bodyResp, []byte("Rewinding")) ||
+				bytes.Contains(bodyResp, []byte("RPC in warm-up: Calculating money supply")) ||
+				bytes.Contains(bodyResp, []byte("Verifying")) {
+				// The wallet is still loading, so print message, and sleep for 1 second and try again..
+				var errStruct GenericRespStruct
+				err = json.Unmarshal(bodyResp, &errStruct)
+				if err != nil {
+					return respStruct, "", err
+				}
+
+				if bytes.Contains(bodyResp, []byte("Loading")) {
+					spin.Message(" Your " + CCoinNameDivi + " wallet is currently Loading, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Rescanning")) {
+					spin.Message(" Your " + CCoinNameDivi + " wallet is currently Rescanning, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Rewinding")) {
+					spin.Message(" Your " + CCoinNameDivi + " wallet is currently Rewinding, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Verifying")) {
+					spin.Message(" Your " + CCoinNameDivi + " wallet is currently Verifying, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Calculating money supply")) {
+					spin.Message(" Your " + CCoinNameDivi + " wallet is currently Calculating money supply, this could take several minutes...")
+				}
+				time.Sleep(1 * time.Second)
+			} else {
+				_ = json.Unmarshal(bodyResp, &respStruct)
+				return respStruct, string(bodyResp), err
+			}
+		}
+	}
+	return respStruct, "", nil
 }
 
 func GetMNSyncStatusDivi(cliConf *ConfStruct) (DiviMNSyncStatusRespStruct, error) {
