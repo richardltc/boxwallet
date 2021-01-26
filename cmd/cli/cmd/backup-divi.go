dd1132322f140d71744f4e8057197770be34d980
richard <richard.mace@gmail.com>
2021-01-26 21:04:08 +0000

    Added tip message to end of Divi backup command

--- cmd/cli/cmd/backup-divi.go
+++ cmd/cli/cmd/backup-divi.go
@@ -34,6 +34,10 @@ var diviCmd = &cobra.Command{
 			log.Fatal("Unable to backup the " + be.CCoinNameDivi + " wallet.dat file: " + err.Error())
 		}
 		fmt.Println("Backup completed. Please store your backup wallet.dat file somewhere safe.")
+		fmt.Println("\n\nThank you for using " + be.CAppName)
+		fmt.Println(be.CAppName + " is FREE to use, and if you'd like to send a tip, please feel free to at the following " + be.CCoinNameDivi + " address below:")
+		s := be.GetTipAddress(be.PTDivi)
+		fmt.Println("\n" + be.CCoinAbbrevDivi + ": " + s)
 	},
 }
 
