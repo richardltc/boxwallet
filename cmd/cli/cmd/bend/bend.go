3b532f3a26554154e88ae89302ae6663bf007280
richard <richard.mace@gmail.com>
2021-02-02 20:54:14 +0000

    Bumped version

--- cmd/cli/cmd/bend/bend.go
+++ cmd/cli/cmd/bend/bend.go
@@ -25,7 +25,7 @@ import (
 const (
 	CAppName        string = "BoxWallet"
 	CUpdaterAppName string = "bwupdater" // bwupdater
-	CBWAppVersion   string = "0.36.3"
+	CBWAppVersion   string = "0.36.4"
 	CAppFilename    string = "boxwallet"
 	CAppFilenameWin string = "boxwallet.exe"
 	CAppLogfile     string = "boxwallet.log"
