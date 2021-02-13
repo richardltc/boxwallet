35bbacb78165574b739671d46e61e799c0351057
richard <richard.mace@gmail.com>
2021-02-13 08:21:33 +0000

    Added listtransactions rpc code for Divi

--- cmd/cli/cmd/bend/divi.go
+++ cmd/cli/cmd/bend/divi.go
@@ -668,6 +668,35 @@ func ListReceivedByAddressDivi(cliConf *ConfStruct, includeZero bool) (DiviListR
 	return respStruct, nil
 }
 
+func ListTransactionsDivi(cliConf *ConfStruct) (DiviListTransactions, error) {
+	var respStruct DiviListTransactions
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandListTransactions + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
 func UnlockWalletDivi(cliConf *ConfStruct, pw string) error {
 	var respStruct GenericRespStruct
 
