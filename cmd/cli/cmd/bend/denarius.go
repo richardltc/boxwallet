b221534a0f1e79a4ad86c99bc7e8642dc7720dfb
richard <richard.mace@gmail.com>
2021-02-28 08:35:08 +0000

    Lots more work on Denarius

--- cmd/cli/cmd/bend/denarius.go
+++ cmd/cli/cmd/bend/denarius.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"github.com/dustin/go-humanize"
+	"github.com/theckman/yacspin"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -34,6 +35,7 @@ const (
 	CDenariusCliFile    string = "denarius"
 	CDenariusCliFileWin string = "denarius-cli.exe"
 	CDenariusDFile      string = "denarius.daemon"
+	CDenariusDMem       string = "denariusd"
 	CDenariusDFileWin   string = "denarius.daemon.exe"
 	CDenariusTxFile     string = "denarius-tx"
 	CDenariusTxFileWin  string = "denarius-tx.exe"
@@ -45,45 +47,15 @@ const (
 
 type DenariusBlockchainInfoRespStruct struct {
 	Result struct {
-		Chain                string  `json:"chain"`
-		Blocks               int     `json:"blocks"`
-		Headers              int     `json:"headers"`
-		Bestblockhash        string  `json:"bestblockhash"`
-		Difficulty           float64 `json:"difficulty"`
-		Mediantime           int     `json:"mediantime"`
-		Verificationprogress float64 `json:"verificationprogress"`
+		Chain         string `json:"chain"`
+		Blocks        int    `json:"blocks"`
+		Bestblockhash string `json:"bestblockhash"`
+		Difficulty    struct {
+			ProofOfWork  float64 `json:"proof-of-work"`
+			ProofOfStake float64 `json:"proof-of-stake"`
+		} `json:"difficulty"`
 		Initialblockdownload bool    `json:"initialblockdownload"`
-		Chainwork            string  `json:"chainwork"`
-		SizeOnDisk           int     `json:"size_on_disk"`
-		Pruned               bool    `json:"pruned"`
-		Softforks            struct {
-			Bip34 struct {
-				Type   string `json:"type"`
-				Active bool   `json:"active"`
-				Height int    `json:"height"`
-			} `json:"bip34"`
-			Bip66 struct {
-				Type   string `json:"type"`
-				Active bool   `json:"active"`
-				Height int    `json:"height"`
-			} `json:"bip66"`
-			Bip65 struct {
-				Type   string `json:"type"`
-				Active bool   `json:"active"`
-				Height int    `json:"height"`
-			} `json:"bip65"`
-			Csv struct {
-				Type   string `json:"type"`
-				Active bool   `json:"active"`
-				Height int    `json:"height"`
-			} `json:"csv"`
-			Segwit struct {
-				Type   string `json:"type"`
-				Active bool   `json:"active"`
-				Height int    `json:"height"`
-			} `json:"segwit"`
-		} `json:"softforks"`
-		Warnings string `json:"warnings"`
+		Moneysupply          float64 `json:"moneysupply"`
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
@@ -126,13 +98,13 @@ type DenariusGetInfoRespStruct struct {
 		Mininput             float64 `json:"mininput"`
 		Datadir              string  `json:"datadir"`
 		Initialblockdownload bool    `json:"initialblockdownload"`
+		UnlockedUntil        int     `json:"unlocked_until"`
 		WalletStatus         string  `json:"wallet_status"`
 		Errors               string  `json:"errors"`
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
 }
-
 type DenariusListReceivedByAddressRespStruct struct {
 	Result []struct {
 		Address       string        `json:"address"`
@@ -145,28 +117,24 @@ type DenariusListReceivedByAddressRespStruct struct {
 	ID    string      `json:"id"`
 }
 
-type DenariusNetworkInfoRespStruct struct {
-	Result struct {
-		Version            int      `json:"version"`
-		Subversion         string   `json:"subversion"`
-		Protocolversion    int      `json:"protocolversion"`
-		Localservices      string   `json:"localservices"`
-		Localservicesnames []string `json:"localservicesnames"`
-		Localrelay         bool     `json:"localrelay"`
-		Timeoffset         int      `json:"timeoffset"`
-		Networkactive      bool     `json:"networkactive"`
-		Connections        int      `json:"connections"`
-		Networks           []struct {
-			Name                      string `json:"name"`
-			Limited                   bool   `json:"limited"`
-			Reachable                 bool   `json:"reachable"`
-			Proxy                     string `json:"proxy"`
-			ProxyRandomizeCredentials bool   `json:"proxy_randomize_credentials"`
-		} `json:"networks"`
-		Relayfee       float64       `json:"relayfee"`
-		Incrementalfee float64       `json:"incrementalfee"`
-		Localaddresses []interface{} `json:"localaddresses"`
-		Warnings       string        `json:"warnings"`
+type DenariusListTransactions struct {
+	Result []struct {
+		InvolvesWatchonly int           `json:"involvesWatchonly"`
+		Address           string        `json:"address"`
+		Amount            float64       `json:"amount"`
+		Vout              int           `json:"vout"`
+		Category          string        `json:"category"`
+		Account           string        `json:"account"`
+		Confirmations     int           `json:"confirmations"`
+		Bcconfirmations   int           `json:"bcconfirmations"`
+		Generated         bool          `json:"generated"`
+		Txid              string        `json:"txid"`
+		Walletconflicts   []interface{} `json:"walletconflicts"`
+		Time              int           `json:"time"`
+		Timereceived      int           `json:"timereceived"`
+		Blockhash         string        `json:"blockhash,omitempty"`
+		Blockindex        int           `json:"blockindex,omitempty"`
+		Blocktime         int           `json:"blocktime,omitempty"`
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
@@ -178,27 +146,45 @@ type DenariusNewAddressStruct struct {
 	ID     string      `json:"id"`
 }
 
-type DenariusWalletInfoRespStruct struct {
+type DenariusStakingInfoStruct struct {
 	Result struct {
-		Walletname            string  `json:"walletname"`
-		Walletversion         int     `json:"walletversion"`
-		Balance               float64 `json:"balance"`
-		UnconfirmedBalance    float64 `json:"unconfirmed_balance"`
-		ImmatureBalance       float64 `json:"immature_balance"`
-		Txcount               int     `json:"txcount"`
-		Keypoololdest         int     `json:"keypoololdest"`
-		Keypoolsize           int     `json:"keypoolsize"`
-		Hdseedid              string  `json:"hdseedid"`
-		KeypoolsizeHdInternal int     `json:"keypoolsize_hd_internal"`
-		Paytxfee              float64 `json:"paytxfee"`
-		PrivateKeysEnabled    bool    `json:"private_keys_enabled"`
-		AvoidReuse            bool    `json:"avoid_reuse"`
-		Scanning              bool    `json:"scanning"`
-		UnlockedUntil         int     `json:"unlocked_until"`
+		Enabled          bool    `json:"enabled"`
+		Staking          bool    `json:"staking"`
+		Errors           string  `json:"errors"`
+		Currentblocksize int     `json:"currentblocksize"`
+		Currentblocktx   int     `json:"currentblocktx"`
+		Pooledtx         int     `json:"pooledtx"`
+		Difficulty       float64 `json:"difficulty"`
+		SearchInterval   int     `json:"search-interval"`
+		Weight           int     `json:"weight"`
+		Netstakeweight   int     `json:"netstakeweight"`
+		Expectedtime     int     `json:"expectedtime"`
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
 }
+
+//type DenariusWalletInfoRespStruct struct {
+//	Result struct {
+//		Walletname            string  `json:"walletname"`
+//		Walletversion         int     `json:"walletversion"`
+//		Balance               float64 `json:"balance"`
+//		UnconfirmedBalance    float64 `json:"unconfirmed_balance"`
+//		ImmatureBalance       float64 `json:"immature_balance"`
+//		Txcount               int     `json:"txcount"`
+//		Keypoololdest         int     `json:"keypoololdest"`
+//		Keypoolsize           int     `json:"keypoolsize"`
+//		Hdseedid              string  `json:"hdseedid"`
+//		KeypoolsizeHdInternal int     `json:"keypoolsize_hd_internal"`
+//		Paytxfee              float64 `json:"paytxfee"`
+//		PrivateKeysEnabled    bool    `json:"private_keys_enabled"`
+//		AvoidReuse            bool    `json:"avoid_reuse"`
+//		Scanning              bool    `json:"scanning"`
+//		UnlockedUntil         int     `json:"unlocked_until"`
+//	} `json:"result"`
+//	Error interface{} `json:"error"`
+//	ID    string      `json:"id"`
+//}
 
 func GetBlockchainInfoDenarius(cliConf *ConfStruct) (DenariusBlockchainInfoRespStruct, error) {
 	var respStruct DenariusBlockchainInfoRespStruct
@@ -274,6 +260,63 @@ func GetInfoDenarius(cliConf *ConfStruct) (DenariusGetInfoRespStruct, error) {
 	return respStruct, nil
 }
 
+func GetInfoDenariusUI(cliConf *ConfStruct, spin *yacspin.Spinner) (DenariusGetInfoRespStruct, string, error) {
+	var respStruct DenariusGetInfoRespStruct
+
+	for i := 1; i < 600; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetInfo + "\",\"params\":[]}")
+		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+		if err != nil {
+			return respStruct, "", err
+		}
+		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		resp, err := http.DefaultClient.Do(req)
+		defer resp.Body.Close()
+		if err != nil {
+			spin.Message(" waiting for your " + CCoinNameDenarius + " wallet to respond, this could take several minutes (ctrl-c to cancel)...")
+			time.Sleep(1 * time.Second)
+		} else {
+			bodyResp, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return respStruct, "", err
+			}
+
+			// Check to make sure we are not loading the wallet
+			if bytes.Contains(bodyResp, []byte("Loading")) ||
+				bytes.Contains(bodyResp, []byte("Rescanning")) ||
+				bytes.Contains(bodyResp, []byte("Rewinding")) ||
+				bytes.Contains(bodyResp, []byte("RPC in warm-up: Calculating money supply")) ||
+				bytes.Contains(bodyResp, []byte("Verifying")) {
+				// The wallet is still loading, so print message, and sleep for 1 second and try again..
+				var errStruct GenericRespStruct
+				err = json.Unmarshal(bodyResp, &errStruct)
+				if err != nil {
+					return respStruct, "", err
+				}
+
+				if bytes.Contains(bodyResp, []byte("Loading")) {
+					spin.Message(" Your " + CCoinNameDenarius + " wallet is *Loading*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Rescanning")) {
+					spin.Message(" Your " + CCoinNameDenarius + " wallet is *Rescanning*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Rewinding")) {
+					spin.Message(" Your " + CCoinNameDenarius + " wallet is *Rewinding*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Verifying")) {
+					spin.Message(" Your " + CCoinNameDenarius + " wallet is *Verifying*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Calculating money supply")) {
+					spin.Message(" Your " + CCoinNameDenarius + " wallet is *Calculating money supply*, this could take a while...")
+				}
+				time.Sleep(1 * time.Second)
+			} else {
+				_ = json.Unmarshal(bodyResp, &respStruct)
+				return respStruct, string(bodyResp), err
+			}
+		}
+	}
+	return respStruct, "", nil
+}
+
 func GetNetworkConnectionsTxtDenarius(connections int) string {
 	if connections == 0 {
 		return "Peers:       [0](fg:red)"
@@ -310,40 +353,40 @@ func GetNewAddressDenarius(cliConf *ConfStruct) (DenariusNewAddressStruct, error
 	return respStruct, nil
 }
 
-func GetWalletInfoDenarius(cliConf *ConfStruct) (DenariusWalletInfoRespStruct, error) {
-	var respStruct DenariusWalletInfoRespStruct
-
-	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getwalletinfo\",\"params\":[]}")
-	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
-	if err != nil {
-		return respStruct, err
-	}
-	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
-	req.Header.Set("Content-Type", "text/plain;")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return respStruct, err
-	}
-	defer resp.Body.Close()
-	bodyResp, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return respStruct, err
-	}
-
-	err = json.Unmarshal(bodyResp, &respStruct)
-	if err != nil {
-		return respStruct, err
-	}
-
-	// Check to see if the json response contains "unlocked_until"
-	s := string([]byte(bodyResp))
-	if !strings.Contains(s, "unlocked_until") {
-		respStruct.Result.UnlockedUntil = -1
-	}
-
-	return respStruct, nil
-}
+//func GetWalletInfoDenarius(cliConf *ConfStruct) (DenariusWalletInfoRespStruct, error) {
+//	var respStruct DenariusWalletInfoRespStruct
+//
+//	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getwalletinfo\",\"params\":[]}")
+//	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+//	if err != nil {
+//		return respStruct, err
+//	}
+//	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+//	req.Header.Set("Content-Type", "text/plain;")
+//
+//	resp, err := http.DefaultClient.Do(req)
+//	if err != nil {
+//		return respStruct, err
+//	}
+//	defer resp.Body.Close()
+//	bodyResp, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		return respStruct, err
+//	}
+//
+//	err = json.Unmarshal(bodyResp, &respStruct)
+//	if err != nil {
+//		return respStruct, err
+//	}
+//
+//	// Check to see if the json response contains "unlocked_until"
+//	s := string([]byte(bodyResp))
+//	if !strings.Contains(s, "unlocked_until") {
+//		respStruct.Result.UnlockedUntil = -1
+//	}
+//
+//	return respStruct, nil
+//}
 
 func GetNetworkBlocksTxtDenarius(bci *DenariusBlockchainInfoRespStruct) string {
 	blocksStr := humanize.Comma(int64(bci.Result.Blocks))
@@ -355,36 +398,36 @@ func GetNetworkBlocksTxtDenarius(bci *DenariusBlockchainInfoRespStruct) string {
 	return "Blocks:      [" + blocksStr + "](fg:green)"
 }
 
-func GetNetworkHeadersTxtDenarius(bci *DenariusBlockchainInfoRespStruct) string {
-	headersStr := humanize.Comma(int64(bci.Result.Headers))
+//func GetNetworkHeadersTxtDenarius(bci *DenariusBlockchainInfoRespStruct) string {
+//	headersStr := humanize.Comma(int64(bci.Result.Headers))
+//
+//	if bci.Result.Headers > 1 {
+//		return "Headers:     [" + headersStr + "](fg:green)"
+//	} else {
+//		return "[Headers:     " + headersStr + "](fg:red)"
+//	}
+//}
 
-	if bci.Result.Headers > 1 {
-		return "Headers:     [" + headersStr + "](fg:green)"
-	} else {
-		return "[Headers:     " + headersStr + "](fg:red)"
-	}
-}
-
-func GetBlockchainSyncTxtDenarius(synced bool, bci *DenariusBlockchainInfoRespStruct) string {
-	s := ConvertBCVerification(bci.Result.Verificationprogress)
-	if s == "0.0" {
-		s = ""
-	} else {
-		s = s + "%"
-	}
-
-	if !synced {
-		if bci.Result.Verificationprogress > gLastBCSyncPos {
-			gLastBCSyncPos = bci.Result.Verificationprogress
-			return "Blockchain:  [syncing " + s + " ](fg:yellow)"
-		} else {
-			gLastBCSyncPos = bci.Result.Verificationprogress
-			return "Blockchain:  [waiting " + s + " ](fg:yellow)"
-		}
-	} else {
-		return "Blockchain:  [synced " + CUtfTickBold + "](fg:green)"
-	}
-}
+//func GetBlockchainSyncTxtDenarius(synced bool, bci *DenariusBlockchainInfoRespStruct) string {
+//	s := ConvertBCVerification(bci.Result.Verificationprogress)
+//	if s == "0.0" {
+//		s = ""
+//	} else {
+//		s = s + "%"
+//	}
+//
+//	if !synced {
+//		if bci.Result.Verificationprogress > gLastBCSyncPos {
+//			gLastBCSyncPos = bci.Result.Verificationprogress
+//			return "Blockchain:  [syncing " + s + " ](fg:yellow)"
+//		} else {
+//			gLastBCSyncPos = bci.Result.Verificationprogress
+//			return "Blockchain:  [waiting " + s + " ](fg:yellow)"
+//		}
+//	} else {
+//		return "Blockchain:  [synced " + CUtfTickBold + "](fg:green)"
+//	}
+//}
 
 func GetNetworkDifficultyTxtDenarius(difficulty, good, warn float64) string {
 	var s string
@@ -408,49 +451,49 @@ func GetNetworkDifficultyTxtDenarius(difficulty, good, warn float64) string {
 	}
 }
 
-func GetNetworkInfoDenarius(cliConf *ConfStruct) (DenariusNetworkInfoRespStruct, error) {
-	var respStruct DenariusNetworkInfoRespStruct
+//func GetNetworkInfoDenarius(cliConf *ConfStruct) (DenariusNetworkInfoRespStruct, error) {
+//	var respStruct DenariusNetworkInfoRespStruct
+//
+//	for i := 1; i < 50; i++ {
+//		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getnetworkinfo\",\"params\":[]}")
+//
+//		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+//		if err != nil {
+//			return respStruct, err
+//		}
+//		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+//		req.Header.Set("Content-Type", "text/plain;")
+//
+//		resp, err := http.DefaultClient.Do(req)
+//		if err != nil {
+//			return respStruct, err
+//		}
+//		defer resp.Body.Close()
+//		bodyResp, err := ioutil.ReadAll(resp.Body)
+//		if err != nil {
+//			return respStruct, err
+//		}
+//
+//		// Check to make sure we are not loading the wallet
+//		if bytes.Contains(bodyResp, []byte("Loading")) ||
+//			bytes.Contains(bodyResp, []byte("Rewinding")) ||
+//			bytes.Contains(bodyResp, []byte("Verifying")) {
+//			// The wallet is still loading, so print message, and sleep for 3 seconds and try again..
+//			time.Sleep(5 * time.Second)
+//		} else {
+//			_ = json.Unmarshal(bodyResp, &respStruct)
+//			return respStruct, err
+//		}
+//	}
+//	return respStruct, nil
+//}
 
-	for i := 1; i < 50; i++ {
-		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getnetworkinfo\",\"params\":[]}")
-
-		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
-		if err != nil {
-			return respStruct, err
-		}
-		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
-		req.Header.Set("Content-Type", "text/plain;")
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return respStruct, err
-		}
-		defer resp.Body.Close()
-		bodyResp, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return respStruct, err
-		}
-
-		// Check to make sure we are not loading the wallet
-		if bytes.Contains(bodyResp, []byte("Loading")) ||
-			bytes.Contains(bodyResp, []byte("Rewinding")) ||
-			bytes.Contains(bodyResp, []byte("Verifying")) {
-			// The wallet is still loading, so print message, and sleep for 3 seconds and try again..
-			time.Sleep(5 * time.Second)
-		} else {
-			_ = json.Unmarshal(bodyResp, &respStruct)
-			return respStruct, err
-		}
-	}
-	return respStruct, nil
-}
-
-func GetWalletSecurityStateDenarius(wi *DenariusWalletInfoRespStruct) WEType {
-	if wi.Result.UnlockedUntil == 0 {
+func GetWalletSecurityStateDenarius(gi *DenariusGetInfoRespStruct) WEType {
+	if gi.Result.UnlockedUntil == 0 {
 		return WETLocked
-	} else if wi.Result.UnlockedUntil == -1 {
+	} else if gi.Result.UnlockedUntil == -1 {
 		return WETUnencrypted
-	} else if wi.Result.UnlockedUntil > 0 {
+	} else if gi.Result.UnlockedUntil > 0 {
 		return WETUnlockedForStaking
 	} else {
 		return WETUnknown
