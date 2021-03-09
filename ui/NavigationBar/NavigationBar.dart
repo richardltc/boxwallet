f4d637c2f62a296e822b031feb8f2b71c2db4e63
richard <richard.mace@gmail.com>
2021-03-09 13:26:45 +0000

    Initial UI commit

---
+++ ui/NavigationBar/NavigationBar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class NavigationBar extends StatefulWidget {
+  @override
+  _NavigationBarState createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.centerLeft,
+          child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: 100.0,
+        color: Color(0xff333951),
+        child: Stack(
+          children:
+            CompanyName(),
+          )
+
+      ) // Stack
+    ); // Container
+  }
+}
---
+++ ui/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp)));
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          children:
+            NavigationBar();
+
+        ) // Stack
+      ) // Container
+    ) // Scaffold
+  }
+}
