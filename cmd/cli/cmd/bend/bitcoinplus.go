6ec9fc7ffeacbb8fd5ee37a66c12fc22e0cdc778
richard <richard.mace@gmail.com>
2021-03-31 13:55:56 +0100

    Refactoring of BitcoinPlus constants

--- cmd/cli/cmd/bend/bitcoinplus.go
+++ cmd/cli/cmd/bend/bitcoinplus.go
@@ -4,35 +4,35 @@ const (
 	CCoinNameBitcoinPlus   string = "BitcoinPlus"
 	CCoinAbbrevBitcoinPlus string = "XBC"
 
-	CBitcoinPlusCoreVersion string = "2.8.2"
-	//CDFBitcoinPlusFileArm32          = "bitcoinplus-" + CBitcoinPlusCoreVersion + "-arm-linux-gnueabihf.tar.gz"
-	//CDFBitcoinPlusFileArm64          = "bitcoinplus-" + CBitcoinPlusCoreVersion + "-aarch64-linux-gnu.tar.gz"
-	CDFBitcoinPlusFileLinux32 = "bitcoinplus-" + CBitcoinPlusCoreVersion + "-linux32.tar.gz"
-	CDFBitcoinPlusFileLinux64 = "bitcoinplus-" + CBitcoinPlusCoreVersion + "-linux64.tar.gz"
-	//CDFBitcoinPlusFilemacOS          = "bitcoinplus-" + CBitcoinPlusCoreVersion + "-osx64.tar.gz"
-	//CDFBitcoinPlusFileWindows        = "bitcoinplus-" + CBitcoinPlusCoreVersion + "-win64.zip"
+	CCoreVersionBitcoinPlus string = "2.8.2"
+	//CDFBitcoinPlusFileArm32          = "bitcoinplus-" + CCoreVersionBitcoinPlus + "-arm-linux-gnueabihf.tar.gz"
+	//CDFBitcoinPlusFileArm64          = "bitcoinplus-" + CCoreVersionBitcoinPlus + "-aarch64-linux-gnu.tar.gz"
+	CDFFileLinux32BitcoinPlus = "bitcoinplus-" + CCoreVersionBitcoinPlus + "-linux32.tar.gz"
+	CDFFileLinux64BitcoinPlus = "bitcoinplus-" + CCoreVersionBitcoinPlus + "-linux64.tar.gz"
+	//CDFBitcoinPlusFilemacOS          = "bitcoinplus-" + CCoreVersionBitcoinPlus + "-osx64.tar.gz"
+	//CDFBitcoinPlusFileWindows        = "bitcoinplus-" + CCoreVersionBitcoinPlus + "-win64.zip"
 
 	// Directory const
-	CBitcoinPlusExtractedDirArm     string = "bitcoinplus-" + CBitcoinPlusCoreVersion + "/"
-	CBitcoinPlusExtractedDirLinux   string = "bitcoinplus-" + CBitcoinPlusCoreVersion + "/"
-	CBitcoinPlusExtractedDirWindows string = "bitcoinplus-" + CBitcoinPlusCoreVersion + "\\"
+	CExtractedDirArmBitcoinPlus     string = "bitcoinplus-" + CCoreVersionBitcoinPlus + "/"
+	CExtractedDirLinuxBitcoinPlus   string = "bitcoinplus-" + CCoreVersionBitcoinPlus + "/"
+	CExtractedDirWindowsBitcoinPlus string = "bitcoinplus-" + CCoreVersionBitcoinPlus + "\\"
 
-	CDownloadURLBitcoinPlus string = "https://github.com/bitcoinplusorg/xbcwalletsource/releases/download/v" + CBitcoinPlusCoreVersion + "/"
+	CDownloadURLBitcoinPlus string = "https://github.com/bitcoinplusorg/xbcwalletsource/releases/download/v" + CCoreVersionBitcoinPlus + "/"
 
 	// BitcoinPlus Wallet Constants
-	cBitcoinPlusHomeDir    string = ".bitcoinplus"
-	cBitcoinPlusHomeDirWin string = "bitcoinplus"
+	cHomeDirLinBitcoinPlus string = ".bitcoinplus"
+	cHomeDirWinBitcoinPlus string = "bitcoinplus"
 
 	// File constants
-	cBitcoinPlusConfFile   string = "bitcoinplus.conf"
-	CBitcoinPlusCliFile    string = "bitcoinplus-cli"
-	CBitcoinPlusCliFileWin string = "bitcoinplus-cli.exe"
-	CBitcoinPlusDFile      string = "bitcoinplusd"
-	CBitcoinPlusDFileWin   string = "bitcoinplusd.exe"
-	CBitcoinPlusTxFile     string = "bitcoinplus-tx"
-	CBitcoinPlusTxFileWin  string = "bitcoinplus-tx.exe"
+	cConfFileBitcoinPlus   string = "bitcoinplus.conf"
+	CCliFileBitcoinPlus    string = "bitcoinplus-cli"
+	CCliFileWinBitcoinPlus string = "bitcoinplus-cli.exe"
+	CDFileBitcoinPlus      string = "bitcoinplusd"
+	CDFileWinBitcoinPlus   string = "bitcoinplusd.exe"
+	CTxFileBitcoinPlus     string = "bitcoinplus-tx"
+	CTxFileWinBitcoinPlus  string = "bitcoinplus-tx.exe"
 
 	// pivx.conf file constants
-	CBitcoinPlusRPCUser string = "bitcoinplusrpc"
-	CBitcoinPlusRPCPort string = "8885"
+	CRPCUserBitcoinPlus string = "bitcoinplusrpc"
+	CRPCPortBitcoinPlus string = "8885"
 )
