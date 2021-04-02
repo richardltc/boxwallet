3245e2eb7d2c1d67740879b0b77fb0f19a368fe7
richard <richard.mace@gmail.com>
2021-04-02 08:36:55 +0100

    Added BitcoinPlus main backend code

--- cmd/cli/cmd/bend/bitcoinplus.go
+++ cmd/cli/cmd/bend/bitcoinplus.go
@@ -1,5 +1,17 @@
 package bend
 
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/dustin/go-humanize"
+	"github.com/theckman/yacspin"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
 const (
 	CCoinNameBitcoinPlus   string = "BitcoinPlus"
 	CCoinAbbrevBitcoinPlus string = "XBC"
@@ -36,3 +48,503 @@ const (
 	CRPCUserBitcoinPlus string = "bitcoinplusrpc"
 	CRPCPortBitcoinPlus string = "8885"
 )
+
+type XBCBlockchainInfoRespStruct struct {
+	Result struct {
+		Chain                string  `json:"chain"`
+		Blocks               int     `json:"blocks"`
+		Headers              int     `json:"headers"`
+		Bestblockhash        string  `json:"bestblockhash"`
+		Difficulty           float64 `json:"difficulty"`
+		Mediantime           int     `json:"mediantime"`
+		Verificationprogress float64 `json:"verificationprogress"`
+		Chainwork            string  `json:"chainwork"`
+		Pruned               bool    `json:"pruned"`
+		Softforks            []struct {
+			ID      string `json:"id"`
+			Version int    `json:"version"`
+			Enforce struct {
+				Status   bool `json:"status"`
+				Found    int  `json:"found"`
+				Required int  `json:"required"`
+				Window   int  `json:"window"`
+			} `json:"enforce"`
+			Reject struct {
+				Status   bool `json:"status"`
+				Found    int  `json:"found"`
+				Required int  `json:"required"`
+				Window   int  `json:"window"`
+			} `json:"reject"`
+		} `json:"softforks"`
+		Bip9Softforks struct {
+			Csv struct {
+				Status    string `json:"status"`
+				StartTime int    `json:"startTime"`
+				Timeout   int    `json:"timeout"`
+			} `json:"csv"`
+			Segwit struct {
+				Status    string `json:"status"`
+				StartTime int    `json:"startTime"`
+				Timeout   int    `json:"timeout"`
+			} `json:"segwit"`
+		} `json:"bip9_softforks"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type xbcGetInfoRespStruct struct {
+	Result struct {
+		Version         int     `json:"version"`
+		Protocolversion int     `json:"protocolversion"`
+		Walletversion   int     `json:"walletversion"`
+		Balance         float64 `json:"balance"`
+		Newmint         float64 `json:"newmint"`
+		Stake           float64 `json:"stake"`
+		Blocks          int     `json:"blocks"`
+		Moneysupply     float64 `json:"moneysupply"`
+		Timeoffset      int     `json:"timeoffset"`
+		Connections     int     `json:"connections"`
+		Proxy           string  `json:"proxy"`
+		Difficulty      struct {
+			ProofOfWork  float64 `json:"proof-of-work"`
+			ProofOfStake float64 `json:"proof-of-stake"`
+		} `json:"difficulty"`
+		Testnet       bool    `json:"testnet"`
+		Keypoololdest int     `json:"keypoololdest"`
+		Keypoolsize   int     `json:"keypoolsize"`
+		Paytxfee      float64 `json:"paytxfee"`
+		Relayfee      float64 `json:"relayfee"`
+		Errors        string  `json:"errors"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type XBCListReceivedByAddressRespStruct struct {
+	Result []struct {
+		Address       string        `json:"address"`
+		Account       string        `json:"account"`
+		Amount        float64       `json:"amount"`
+		Confirmations int           `json:"confirmations"`
+		Label         string        `json:"label"`
+		Txids         []interface{} `json:"txids"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type XBCListTransactions struct {
+	Result []struct {
+		Account         string        `json:"account"`
+		Address         string        `json:"address"`
+		Category        string        `json:"category"`
+		Amount          float64       `json:"amount"`
+		Vout            int           `json:"vout,omitempty"`
+		Confirmations   int           `json:"confirmations"`
+		Blockhash       string        `json:"blockhash"`
+		Blockindex      int           `json:"blockindex"`
+		Blocktime       int           `json:"blocktime"`
+		Txid            string        `json:"txid"`
+		Walletconflicts []interface{} `json:"walletconflicts"`
+		Time            int           `json:"time"`
+		Timereceived    int           `json:"timereceived"`
+		Generated       bool          `json:"generated,omitempty"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type XBCNetworkInfoRespStruct struct {
+	Result struct {
+		Version         int    `json:"version"`
+		Subversion      string `json:"subversion"`
+		Protocolversion int    `json:"protocolversion"`
+		Localservices   string `json:"localservices"`
+		Localrelay      bool   `json:"localrelay"`
+		Timeoffset      int    `json:"timeoffset"`
+		Connections     int    `json:"connections"`
+		Networks        []struct {
+			Name                      string `json:"name"`
+			Limited                   bool   `json:"limited"`
+			Reachable                 bool   `json:"reachable"`
+			Proxy                     string `json:"proxy"`
+			ProxyRandomizeCredentials bool   `json:"proxy_randomize_credentials"`
+		} `json:"networks"`
+		Relayfee       float64       `json:"relayfee"`
+		Localaddresses []interface{} `json:"localaddresses"`
+		Warnings       string        `json:"warnings"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type XBCStakingInfoRespStruct struct {
+	Result struct {
+		Enabled          bool    `json:"enabled"`
+		Staking          bool    `json:"staking"`
+		Errors           string  `json:"errors"`
+		Currentblocksize int     `json:"currentblocksize"`
+		Currentblocktx   int     `json:"currentblocktx"`
+		Difficulty       float64 `json:"difficulty"`
+		SearchInterval   int     `json:"search-interval"`
+		Weight           int     `json:"weight"`
+		Netstakeweight   int     `json:"netstakeweight"`
+		Expectedtime     int     `json:"expectedtime"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type XBCWalletInfoRespStruct struct {
+	Result struct {
+		Walletversion      int     `json:"walletversion"`
+		Balance            float64 `json:"balance"`
+		UnconfirmedBalance float64 `json:"unconfirmed_balance"`
+		ImmatureBalance    float64 `json:"immature_balance"`
+		Txcount            int     `json:"txcount"`
+		Keypoololdest      int     `json:"keypoololdest"`
+		Keypoolsize        int     `json:"keypoolsize"`
+		Paytxfee           float64 `json:"paytxfee"`
+		UnlockedUntil      int     `json:"unlocked_until"`
+		Hdmasterkeyid      string  `json:"hdmasterkeyid"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+func GetBlockchainInfoXBC(cliConf *ConfStruct) (XBCBlockchainInfoRespStruct, error) {
+	var respStruct XBCBlockchainInfoRespStruct
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetBCInfo + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func GetInfoXBC(cliConf *ConfStruct) (xbcGetInfoRespStruct, string, error) {
+	var respStruct xbcGetInfoRespStruct
+
+	for i := 1; i < 300; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetInfo + "\",\"params\":[]}")
+		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+		if err != nil {
+			return respStruct, "", err
+		}
+		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		for j := 1; j < 50; j++ {
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			defer resp.Body.Close()
+			bodyResp, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return respStruct, "", err
+			}
+
+			// Check to make sure we are not loading the wallet
+			if bytes.Contains(bodyResp, []byte("Loading")) ||
+				bytes.Contains(bodyResp, []byte("Rescanning")) ||
+				bytes.Contains(bodyResp, []byte("Rewinding")) ||
+				bytes.Contains(bodyResp, []byte("Verifying")) {
+				// The wallet is still loading, so print message, and sleep for 3 seconds and try again.
+				var errStruct GenericRespStruct
+				err = json.Unmarshal(bodyResp, &errStruct)
+				if err != nil {
+					return respStruct, "", err
+				}
+				time.Sleep(1 * time.Second)
+			} else {
+
+				_ = json.Unmarshal(bodyResp, &respStruct)
+				return respStruct, string(bodyResp), err
+			}
+		}
+	}
+	return respStruct, "", nil
+}
+
+func GetInfoXBCUI(cliConf *ConfStruct, spin *yacspin.Spinner) (xbcGetInfoRespStruct, string, error) {
+	var respStruct xbcGetInfoRespStruct
+
+	for i := 1; i < 600; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetInfo + "\",\"params\":[]}")
+		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+		if err != nil {
+			return respStruct, "", err
+		}
+		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		resp, err := http.DefaultClient.Do(req)
+		//defer resp.Body.Close()
+		if err != nil {
+			spin.Message(" waiting for your " + CCoinNameBitcoinPlus + " wallet to respond, this could take several minutes (ctrl-c to cancel)...")
+			time.Sleep(1 * time.Second)
+		} else {
+			defer resp.Body.Close()
+			bodyResp, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return respStruct, "", err
+			}
+
+			// Check to make sure we are not loading the wallet
+			if bytes.Contains(bodyResp, []byte("Loading")) ||
+				bytes.Contains(bodyResp, []byte("Rescanning")) ||
+				bytes.Contains(bodyResp, []byte("Rewinding")) ||
+				bytes.Contains(bodyResp, []byte("RPC in warm-up: Calculating money supply")) ||
+				bytes.Contains(bodyResp, []byte("Verifying")) {
+				// The wallet is still loading, so print message, and sleep for 1 second and try again..
+				var errStruct GenericRespStruct
+				err = json.Unmarshal(bodyResp, &errStruct)
+				if err != nil {
+					return respStruct, "", err
+				}
+
+				if bytes.Contains(bodyResp, []byte("Loading")) {
+					spin.Message(" Your " + CCoinNameBitcoinPlus + " wallet is *Loading*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Rescanning")) {
+					spin.Message(" Your " + CCoinNameBitcoinPlus + " wallet is *Rescanning*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Rewinding")) {
+					spin.Message(" Your " + CCoinNameBitcoinPlus + " wallet is *Rewinding*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Verifying")) {
+					spin.Message(" Your " + CCoinNameBitcoinPlus + " wallet is *Verifying*, this could take a while...")
+				} else if bytes.Contains(bodyResp, []byte("Calculating money supply")) {
+					spin.Message(" Your " + CCoinNameBitcoinPlus + " wallet is *Calculating money supply*, this could take a while...")
+				}
+				time.Sleep(1 * time.Second)
+			} else {
+				_ = json.Unmarshal(bodyResp, &respStruct)
+				return respStruct, string(bodyResp), err
+			}
+		}
+	}
+	return respStruct, "", nil
+}
+
+func GetNetworkConnectionsTxtXBC(connections int) string {
+	if connections == 0 {
+		return "Peers:       [0](fg:red)"
+	}
+	return "Peers:       [" + strconv.Itoa(connections) + "](fg:green)"
+}
+
+func GetNetworkDifficultyTxtXBC(difficulty, good, warn float64) string {
+	var s string
+	if difficulty > 1000 {
+		s = humanize.FormatFloat("#.#", difficulty/1000) + "k"
+	} else {
+		s = humanize.Ftoa(difficulty)
+	}
+
+	// If Diff is less than 1, then we're not even calculating it properly yet...
+	if difficulty < 1 {
+		return "[Difficulty:  waiting...](fg:white)"
+	}
+
+	if difficulty >= good {
+		return "Difficulty:  [" + s + "](fg:green)"
+	} else if difficulty >= warn {
+		return "Difficulty:  [" + s + "](fg:yellow)"
+	} else {
+		return "Difficulty:  [" + s + "](fg:red)"
+	}
+}
+
+func GetNetworkInfoXBC(cliConf *ConfStruct) (XBCNetworkInfoRespStruct, error) {
+	var respStruct XBCNetworkInfoRespStruct
+
+	for i := 1; i < 50; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetNetworkInfo + "\",\"params\":[]}")
+
+		req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+		if err != nil {
+			return respStruct, err
+		}
+		req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return respStruct, err
+		}
+		defer resp.Body.Close()
+		bodyResp, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return respStruct, err
+		}
+
+		// Check to make sure we are not loading the wallet
+		if bytes.Contains(bodyResp, []byte("Loading")) ||
+			bytes.Contains(bodyResp, []byte("Rescanning")) ||
+			bytes.Contains(bodyResp, []byte("Rewinding")) ||
+			bytes.Contains(bodyResp, []byte("Verifying")) {
+			// The wallet is still loading, so print message, and sleep for 3 seconds and try again
+			time.Sleep(5 * time.Second)
+		} else {
+			_ = json.Unmarshal(bodyResp, &respStruct)
+			return respStruct, err
+		}
+	}
+	return respStruct, nil
+}
+
+func GetStakingStatusXBC(cliConf *ConfStruct) (XBCStakingInfoRespStruct, error) {
+	var respStruct XBCStakingInfoRespStruct
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getstakingstatus\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func GetWalletInfoXBC(cliConf *ConfStruct) (XBCWalletInfoRespStruct, error) {
+	var respStruct XBCWalletInfoRespStruct
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandGetWalletInfo + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	// Check to see if the json response contains "unlocked_until"
+	s := string(bodyResp)
+	if !strings.Contains(s, "unlocked_until") {
+		respStruct.Result.UnlockedUntil = -1
+	}
+
+	return respStruct, nil
+}
+
+func GetWalletSecurityStateXBC(gi *XBCWalletInfoRespStruct) WEType {
+	if gi.Result.UnlockedUntil == 0 {
+		return WETLocked
+	} else if gi.Result.UnlockedUntil == -1 {
+		return WETUnencrypted
+	} else if gi.Result.UnlockedUntil > 0 {
+		return WETUnlockedForStaking
+	} else {
+		return WETUnknown
+	}
+}
+
+func ListReceivedByAddressXBC(cliConf *ConfStruct, includeZero bool) (XBCListReceivedByAddressRespStruct, error) {
+	var respStruct XBCListReceivedByAddressRespStruct
+
+	var s string
+	if includeZero {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"" + cCommandListReceivedByAddress + "\",\"params\":[1, true]}"
+	} else {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"" + cCommandListReceivedByAddress + "\",\"params\":[1, false]}"
+	}
+	body := strings.NewReader(s)
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func ListTransactionsXBC(cliConf *ConfStruct) (XBCListTransactions, error) {
+	var respStruct XBCListTransactions
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + cCommandListTransactions + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+cliConf.ServerIP+":"+cliConf.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(cliConf.RPCuser, cliConf.RPCpassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
