7746f54aa23befb234e1b76dce56dbaf71d97704
richard <richard.mace@gmail.com>
2021-05-28 06:46:53 +0100

    Added refactored file.

---
+++ cmd/cli/cmd/coins/digibyte/digibyte.go
@@ -0,0 +1,649 @@
+package bend
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/user"
+	"richardmace.co.uk/boxwallet/cmd/cli/cmd/coins"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/mholt/archiver"
+	"github.com/theckman/yacspin"
+	"richardmace.co.uk/boxwallet/cmd/cli/cmd/models"
+	"richardmace.co.uk/boxwallet/cmd/cli/cmd/rjminternet"
+)
+
+const (
+	cCoinName       string = "DigiByte"
+	cCoinNameAbbrev string = "DGB"
+
+	cCoreVersion       string = "7.17.2"
+	cDownloadFileArm64 string = "digibyte-" + cCoreVersion + "-aarch64-linux-gnu.tar.gz"
+	cDownloadFileLin   string = "digibyte-" + cCoreVersion + "-x86_64-linux-gnu.tar.gz"
+	cDownloadFileWin   string = "digibyte-" + cCoreVersion + "-win64.zip"
+
+	cExtractedDirLin = "digibyte-" + cCoreVersion + "/"
+	cExtractedDirWin = "digibyte-" + cCoreVersion + "\\"
+
+	cDownloadURL string = "https://github.com/digibyte/digibyte/releases/download/v" + cCoreVersion + "/"
+
+	cHomeDirLin string = ".digibyte"
+	cHomeDirWin string = "DIGIBYTE"
+
+	cTipAddress string = "dgb1qdw7qhh5crt3rhfau909pmc9r0esnnzqf48un6g"
+
+	cConfFile      string = "digibyte.conf"
+	cCliFileLin    string = "digibyte-cli"
+	cCliFileWin    string = "digibyte-cli.exe"
+	cDaemonFileLin string = "digibyted"
+	cDaemonFileWin string = "digibyted.exe"
+	cTxFileLin     string = "digibyte-tx"
+	cTxFileWin     string = "digibyte-tx.exe"
+
+	cRPCUser string = "digibyterpc"
+	cRPCPort string = "14022"
+)
+
+type DigiByte struct {
+	RPCUser     string
+	RPCPassword string
+	IPAddress   string
+	Port        string
+}
+
+func (d DigiByte) BootStrap(rpcUser, rpcPassword, ip, port string) {
+	d.RPCUser = rpcUser
+	d.RPCPassword = rpcPassword
+	d.IPAddress = ip
+	d.Port = port
+}
+
+func (d DigiByte) AbbreviatedCoinName() string {
+	return cCoinNameAbbrev
+}
+
+func (d DigiByte) AllBinaryFilesExist(dir string) (bool, error) {
+	if runtime.GOOS == "windows" {
+		if !coins.FileExists(dir + cCliFileWin) {
+			return false, nil
+		}
+		if !coins.FileExists(dir + cDaemonFileWin) {
+			return false, nil
+		}
+		if !coins.FileExists(dir + cTxFileWin) {
+			return false, nil
+		}
+	} else {
+		if !coins.FileExists(dir + cCliFileLin) {
+			return false, nil
+		}
+		if !coins.FileExists(dir + cDaemonFileLin) {
+			return false, nil
+		}
+		if !coins.FileExists(dir + cTxFileLin) {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func (d DigiByte) ConfFile() string {
+	return cConfFile
+}
+
+func (d DigiByte) CoinName() string {
+	return cCoinName
+}
+
+func (d DigiByte) CoinNameAbbrev() string {
+	return cCoinNameAbbrev
+}
+
+// DownloadCoin - Downloads the Syscoin files into the spcified location.
+// "location" should just be the AppBinaryFolder ~/.boxwallet
+func (d DigiByte) DownloadCoin(location string) error {
+	var fullFilePath, fullFileDLURL string
+
+	switch runtime.GOOS {
+	case "windows":
+		fullFilePath = location + cDownloadFileWin
+		fullFileDLURL = cDownloadURL + cDownloadFileWin
+	case "linux":
+		switch runtime.GOARCH {
+		case "arm":
+			return errors.New("arm32 is not currently supported for :" + cCoinName)
+		case "arm64":
+			fullFilePath = location + cDownloadFileArm64
+			fullFileDLURL = cDownloadURL + cDownloadFileArm64
+		case "386":
+			fullFilePath = location + cDownloadFileLin
+			fullFileDLURL = cDownloadURL + cDownloadFileLin
+		case "amd64":
+			fullFilePath = location + cDownloadFileLin
+			fullFileDLURL = cDownloadURL + cDownloadFileLin
+		}
+	}
+
+	if err := rjminternet.DownloadFile(fullFilePath, fullFileDLURL); err != nil {
+		return fmt.Errorf("unable to download file: %v - %v", fullFilePath+fullFileDLURL, err)
+	}
+
+	// Unarchive the files
+	if err := d.unarchiveFile(fullFilePath, location); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d DigiByte) DaemonFilename() string {
+	if runtime.GOOS == "windows" {
+		return cDaemonFileWin
+	} else {
+		return cDaemonFileLin
+	}
+}
+
+// No GetInfo in DigiByte
+// This call was removed in version 6.16.0. Use the appropriate fields from:
+// getblockchaininfo: blocks, difficulty, chain
+// getnetworkinfo: version, protocolversion, timeoffset, connections, proxy, relayfee, warnings
+// getwalletinfo: balance, keypoololdest, keypoolsize, paytxfee, unlocked_until, walletversion
+
+func (d *DigiByte) BlockchainInfo() (models.DGBBlockchainInfo, error) {
+	var respStruct models.DGBBlockchainInfo
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandGetBCInfo + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func (d *DigiByte) NetworkInfo() (models.DGBNetworkInfo, error) {
+	var respStruct models.DGBNetworkInfo
+
+	for i := 1; i < 50; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandGetNetworkInfo + "\",\"params\":[]}")
+
+		req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+		if err != nil {
+			return respStruct, err
+		}
+		req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return respStruct, err
+		}
+		defer resp.Body.Close()
+		bodyResp, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return respStruct, err
+		}
+
+		// Check to make sure we are not loading the wallet
+		if bytes.Contains(bodyResp, []byte("Loading")) ||
+			bytes.Contains(bodyResp, []byte("Rescanning")) ||
+			bytes.Contains(bodyResp, []byte("Rewinding")) ||
+			bytes.Contains(bodyResp, []byte("Verifying")) {
+			// The wallet is still loading, so print message, and sleep for 3 seconds and try again
+			time.Sleep(5 * time.Second)
+		} else {
+			_ = json.Unmarshal(bodyResp, &respStruct)
+			return respStruct, err
+		}
+	}
+	return respStruct, nil
+}
+
+func (d *DigiByte) NetworkInfoUI(spin *yacspin.Spinner) (models.DGBNetworkInfo, string, error) {
+	var respStruct models.DGBNetworkInfo
+
+	for i := 1; i < 600; i++ {
+		body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandGetNetworkInfo + "\",\"params\":[]}")
+		req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+		if err != nil {
+			return respStruct, "", err
+		}
+		req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+		req.Header.Set("Content-Type", "text/plain;")
+
+		for j := 1; j < 60; j++ {
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				spin.Message(" waiting for your " + cCoinName + " wallet to respond, this could take several minutes (ctrl-c to cancel)...")
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			defer resp.Body.Close()
+			bodyResp, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return respStruct, "", err
+			}
+
+			// Check to make sure we are not loading the wallet
+			if bytes.Contains(bodyResp, []byte("Loading")) ||
+				bytes.Contains(bodyResp, []byte("Rescanning")) ||
+				bytes.Contains(bodyResp, []byte("Rewinding")) ||
+				bytes.Contains(bodyResp, []byte("RPC in warm-up: Calculating money supply")) ||
+				bytes.Contains(bodyResp, []byte("Verifying")) {
+				// The wallet is still loading, so print message, and sleep for 1 second and try again..
+				var errStruct models.GenericResponse
+				err = json.Unmarshal(bodyResp, &errStruct)
+				if err != nil {
+					return respStruct, "", err
+				}
+
+				if bytes.Contains(bodyResp, []byte("Loading")) {
+					spin.Message(" Your " + cCoinName + " wallet is currently Loading, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Rescanning")) {
+					spin.Message(" Your " + cCoinName + " wallet is currently Rescanning, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Rewinding")) {
+					spin.Message(" Your " + cCoinName + " wallet is currently Rewinding, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Verifying")) {
+					spin.Message(" Your " + cCoinName + " wallet is currently Verifying, this could take several minutes...")
+				} else if bytes.Contains(bodyResp, []byte("Calculating money supply")) {
+					spin.Message(" Your " + cCoinName + " wallet is currently Calculating money supply, this could take several minutes...")
+				}
+				time.Sleep(1 * time.Second)
+			} else {
+				_ = json.Unmarshal(bodyResp, &respStruct)
+				return respStruct, string(bodyResp), err
+			}
+		}
+	}
+	return respStruct, "", nil
+}
+
+func (d *DigiByte) NewAddress() (models.DGBGetNewAddress, error) {
+	var respStruct models.DGBGetNewAddress
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandGetNewAddress + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func (d *DigiByte) WalletInfo() (models.DGBWalletInfo, error) {
+	var respStruct models.DGBWalletInfo
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandGetWalletInfo + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	// Check to see if the json response contains "unlocked_until"
+	s := string(bodyResp)
+	if !strings.Contains(s, "unlocked_until") {
+		respStruct.Result.UnlockedUntil = -1
+	}
+
+	return respStruct, nil
+}
+
+func (d *DigiByte) WalletSecurityState() (models.WEType, error) {
+	wi, err := d.WalletInfo()
+	if err != nil {
+		return models.WETUnknown, errors.New("Unable to GetWalletSecurityState: " + err.Error())
+	}
+
+	if wi.Result.UnlockedUntil == 0 {
+		return models.WETLocked, nil
+	} else if wi.Result.UnlockedUntil == -1 {
+		return models.WETUnencrypted, nil
+	} else if wi.Result.UnlockedUntil > 0 {
+		return models.WETUnlockedForStaking, nil
+	} else {
+		return models.WETUnknown, nil
+	}
+}
+
+func (d DigiByte) HomeDir() string {
+	if runtime.GOOS == "windows" {
+		return cHomeDirWin
+	} else {
+		return cHomeDirLin
+	}
+}
+
+func (d DigiByte) HomeDirFullPath() (string, error) {
+	u, err := user.Current()
+	if err != nil {
+		return "", err
+	}
+	hd := u.HomeDir
+
+	if runtime.GOOS == "windows" {
+		return coins.AddTrailingSlash(hd) + "appdata\\roaming\\" + coins.AddTrailingSlash(cHomeDirWin), nil
+	} else {
+		return coins.AddTrailingSlash(hd) + coins.AddTrailingSlash(cHomeDirLin), nil
+	}
+}
+
+// Install - Puts the freshly downloaded files into their correct location.
+// "location" should just be the AppBinaryFolder ~/.boxwallet
+func (d DigiByte) Install(location string) error {
+
+	// Copy files to correct location
+	var srcPath, sfCLI, sfD, sfTX string
+
+	switch runtime.GOOS {
+	case "windows":
+		srcPath = location + cDownloadFileWin
+		sfCLI = cCliFileWin
+		sfD = cDaemonFileWin
+		sfTX = cTxFileWin
+	case "linux":
+		switch runtime.GOARCH {
+		case "arm", "arm64":
+			srcPath = location + cExtractedDirLin
+			sfCLI = cCliFileLin
+			sfD = cDaemonFileLin
+			sfTX = cTxFileLin
+		case "amd64":
+			srcPath = location + cExtractedDirLin
+			sfCLI = cCliFileLin
+			sfD = cDaemonFileLin
+			sfTX = cTxFileLin
+		default:
+			return errors.New("unable to determine runtime.GOARCH " + runtime.GOARCH)
+		}
+	default:
+		return errors.New("unable to determine runtime.GOOS")
+	}
+
+	// If the coin-cli file doesn't already exists the copy it.
+	if _, err := os.Stat(location + sfCLI); os.IsNotExist(err) {
+		if err := coins.FileCopy(srcPath+sfCLI, location+sfCLI, false); err != nil {
+			return fmt.Errorf("unable to copyFile from: %v to %v - %v", srcPath+sfCLI, location+sfCLI, err)
+		}
+	}
+	if err := os.Chmod(location+sfCLI, 0777); err != nil {
+		return fmt.Errorf("unable to chmod file: %v - %v", location+sfCLI, err)
+	}
+
+	// If the coind file doesn't already exists the copy it.
+	if _, err := os.Stat(location + sfD); os.IsNotExist(err) {
+		if err := coins.FileCopy(srcPath+sfD, location+sfD, false); err != nil {
+			return fmt.Errorf("unable to copyFile from: %v to %v - %v", srcPath+sfD, location+sfD, err)
+		}
+	}
+	if err := os.Chmod(location+sfD, 0777); err != nil {
+		return fmt.Errorf("unable to chmod file: %v - %v", location+sfD, err)
+	}
+
+	// If the coitx file doesn't already exists the copy it.
+	if _, err := os.Stat(location + sfTX); os.IsNotExist(err) {
+		if err := coins.FileCopy(srcPath+sfTX, location+sfTX, false); err != nil {
+			return fmt.Errorf("unable to copyFile from: %v to %v - %v", srcPath+sfTX, location+sfTX, err)
+		}
+	}
+	if err := os.Chmod(location+sfTX, 0777); err != nil {
+		return fmt.Errorf("unable to chmod file: %v - %v", location+sfTX, err)
+	}
+
+	return nil
+}
+
+func (d *DigiByte) ListReceivedByAddress(includeZero bool) (models.DGBListReceivedByAddress, error) {
+	var respStruct models.DGBListReceivedByAddress
+
+	var s string
+	if includeZero {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"" + models.CCommandListReceivedByAddress + "\",\"params\":[1, true]}"
+	} else {
+		s = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"" + models.CCommandListReceivedByAddress + "\",\"params\":[1, false]}"
+	}
+	body := strings.NewReader(s)
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func (d *DigiByte) ListTransactions() (models.DGBListTransactions, error) {
+	var respStruct models.DGBListTransactions
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandListTransactions + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func (d DigiByte) RPCDefaultUsername() string {
+	return cRPCUser
+}
+
+func (d DigiByte) RPCDefaultPort() string {
+	return cRPCPort
+}
+
+func (d *DigiByte) StartDaemon(displayOutput bool) error {
+	if runtime.GOOS == "windows" {
+		fp := cHomeDirWin + cDaemonFileWin
+		cmd := exec.Command("cmd.exe", "/C", "start", "/b", fp)
+		if err := cmd.Run(); err != nil {
+			return err
+		}
+	} else {
+		if displayOutput {
+			fmt.Println("Attempting to run the digibyted daemon...")
+		}
+
+		cmdRun := exec.Command(cHomeDirLin + cDaemonFileLin)
+		stdout, err := cmdRun.StdoutPipe()
+		if err != nil {
+			return err
+		}
+		err = cmdRun.Start()
+		if err != nil {
+			return err
+		}
+
+		buf := bufio.NewReader(stdout)
+		num := 1
+		for {
+			line, _, _ := buf.ReadLine()
+			if num > 3 {
+				os.Exit(0)
+			}
+			num++
+			if string(line) == "" { //"DigiByte server starting" {
+				if displayOutput {
+					fmt.Println("DigiByte server starting")
+				}
+				return nil
+			} else {
+				return errors.New("unable to start DigiByte server: " + string(line))
+			}
+		}
+	}
+	return nil
+}
+
+func (d *DigiByte) StopDaemon(displayOut bool) (models.GenericResponse, error) {
+	var respStruct models.GenericResponse
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"stop\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func (d DigiByte) TipAddress() string {
+	return cTipAddress
+}
+
+func (d *DigiByte) UnlockWallet(pw string) error {
+	var respStruct models.GenericResponse
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"walletpassphrase\",\"params\":[\"" + pw + "\",0]}")
+	req, err := http.NewRequest("POST", "http://"+d.IPAddress+":"+d.Port, body)
+	if err != nil {
+		return err
+	}
+	req.SetBasicAuth(d.RPCUser, d.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *DigiByte) unarchiveFile(fullFilePath, location string) error {
+	if err := archiver.Unarchive(fullFilePath, location); err != nil {
+		return fmt.Errorf("unable to unarchive file: %v - %v", fullFilePath, err)
+	}
+	switch runtime.GOOS {
+	case "windows":
+		defer os.RemoveAll(location + cDownloadFileWin)
+	case "linux":
+		switch runtime.GOARCH {
+		case "arm":
+			return errors.New("arm32 is not currently supported for :" + cCoinName)
+		case "arm64":
+			defer os.RemoveAll(location + cDownloadFileArm64)
+		case "386":
+			return errors.New("linux 386 is not currently supported for :" + cCoinName)
+		case "amd64":
+			defer os.RemoveAll(location + cDownloadFileLin)
+		}
+	}
+
+	defer os.Remove(fullFilePath)
+
+	return nil
+}
