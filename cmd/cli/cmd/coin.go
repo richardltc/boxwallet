d4d3620ad09b1666cd7e058112eaa6c86126a18b
richard <richard.mace@gmail.com>
2021-06-08 21:23:06 +0100

    Refactoring.

--- cmd/cli/cmd/coin.go
+++ cmd/cli/cmd/coin.go
@@ -19,6 +19,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	dvt "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/devault"
 
 	"github.com/AlecAivazis/survey/v2"
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/app"
@@ -26,9 +27,18 @@ import (
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/coins"
 	xbc "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/bitcoinplus"
 	denarius "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/denarius"
+	dgb "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/digibyte"
 	divi "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/divi"
-	reddcoin "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/reddcoin"
-	sysCoin "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/syscoin"
+	ftc "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/feathercoin"
+	grs "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/groestlcoin"
+	ppc "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/peercoin"
+	phr "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/phore"
+	pivx "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/pivx"
+	rpd "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/rapids"
+	rdd "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/reddcoin"
+	sys "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/syscoin"
+	tzc "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/trezarcoin"
+	vtc "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/vertcoin"
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/conf"
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/database"
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/models"
@@ -45,7 +55,7 @@ var coinCmd = &cobra.Command{
 		var app app.App
 		fmt.Println("  ____          __          __   _ _      _   \n |  _ \\         \\ \\        / /  | | |    | |  \n | |_) | _____  _\\ \\  /\\  / /_ _| | | ___| |_ \n |  _ < / _ \\ \\/ /\\ \\/  \\/ / _` | | |/ _ \\ __|\n | |_) | (_) >  <  \\  /\\  / (_| | | |  __/ |_ \n |____/ \\___/_/\\_\\  \\/  \\/ \\__,_|_|_|\\___|\\__| v" + app.Version() + "\n                                              \n                                               ")
 
-		SelectedCoin := ""
+		selectedCoin := ""
 		logFile, _ := app.HomeFolder()
 		logFile = logFile + be.CAppLogfile
 
@@ -57,7 +67,7 @@ var coinCmd = &cobra.Command{
 
 		be.AddToLog(logFile, "coin command invoked", false)
 		var cn coins.CoinName
-		cn = sysCoin.Syscoin{}
+		cn = sys.Syscoin{}
 		s := cn.CoinName()
 
 		fmt.Printf(s)
@@ -73,19 +83,21 @@ var coinCmd = &cobra.Command{
 				coins.CCoinNameDigiByte,
 				coins.CCoinNameFeathercoin,
 				coins.CCoinNameGroestlcoin,
+				coins.CCoinNamePeercoin,
 				coins.CCoinNamePhore,
 				coins.CCoinNamePIVX,
 				coins.CCoinNameRapids,
 				coins.CCoinNameReddCoin,
 				coins.CCoinNameScala,
+				coins.CCoinNameSyscoin,
 				coins.CCoinNameTrezarcoin,
 				coins.CCoinNameVertcoin},
 		}
-		survey.AskOne(prompt, &SelectedCoin)
+		survey.AskOne(prompt, &selectedCoin)
 
 		var coin coins.Coin
 		var coinName coins.CoinName
-		var coinRPCDefaults coins.CoinRPCDefaults
+		var coinRPC coins.CoinRPCDefaults
 
 		var cliConf models.Conf
 		var conf conf.Conf
@@ -94,41 +106,83 @@ var coinCmd = &cobra.Command{
 		var err error
 		ipAddress = "127.0.0.1"
 
-		switch SelectedCoin {
+		switch selectedCoin {
 		case coins.CCoinNameBitcoinPlus:
 			coin = xbc.XBC{}
 			coinType = models.PTBitcoinPlus
-			coinRPCDefaults = xbc.XBC{}
+			coinRPC = xbc.XBC{}
 			coinName = xbc.XBC{}
 		case coins.CCoinNameDenarius:
+			coin = denarius.Denarius{}
+			coinName = denarius.Denarius{}
+			coinRPC = denarius.Denarius{}
 			coinType = models.PTDenarius
 		case coins.CCoinNameDeVault:
+			coin = dvt.DeVault{}
+			coinName = dvt.DeVault{}
+			coinRPC = dvt.DeVault{}
 			coinType = models.PTDeVault
 		case coins.CCoinNameDigiByte:
+			coin = dgb.DigiByte{}
+			coinName = dgb.DigiByte{}
+			coinRPC = dgb.DigiByte{}
 			coinType = models.PTDigiByte
 		case coins.CCoinNameDivi:
+			coin = divi.Divi{}
+			coinName = divi.Divi{}
+			coinRPC = divi.Divi{}
 			coinType = models.PTDivi
 		case coins.CCoinNameFeathercoin:
+			coin = ftc.Feathercoin{}
+			coinName = ftc.Feathercoin{}
+			coinRPC = ftc.Feathercoin{}
 			coinType = models.PTFeathercoin
 		case coins.CCoinNameGroestlcoin:
+			coin = grs.Groestlcoin{}
+			coinName = grs.Groestlcoin{}
+			coinRPC = grs.Groestlcoin{}
 			coinType = models.PTGroestlcoin
 		case coins.CCoinNamePeercoin:
+			coin = ppc.Peercoin{}
+			coinName = ppc.Peercoin{}
+			coinRPC = ppc.Peercoin{}
 			coinType = models.PTPeercoin
 		case coins.CCoinNamePhore:
+			coin = phr.Phore{}
+			coinName = phr.Phore{}
+			coinRPC = phr.Phore{}
 			coinType = models.PTPhore
 		case coins.CCoinNamePIVX:
+			coin = pivx.PIVX{}
+			coinName = pivx.PIVX{}
+			coinRPC = pivx.PIVX{}
 			coinType = models.PTPIVX
 		case coins.CCoinNameRapids:
+			coin = rpd.Rapids{}
+			coinName = rpd.Rapids{}
+			coinRPC = rpd.Rapids{}
 			coinType = models.PTRapids
 		case coins.CCoinNameReddCoin:
+			coin = rdd.ReddCoin{}
+			coinName = rdd.ReddCoin{}
+			coinRPC = rdd.ReddCoin{}
 			coinType = models.PTReddCoin
 		case coins.CCoinNameScala:
 			coinType = models.PTScala
 		case coins.CCoinNameSyscoin:
+			coin = sys.Syscoin{}
+			coinName = sys.Syscoin{}
+			coinRPC = sys.Syscoin{}
 			coinType = models.PTSyscoin
 		case coins.CCoinNameTrezarcoin:
+			coin = tzc.Trezarcoin{}
+			coinName = tzc.Trezarcoin{}
+			coinRPC = tzc.Trezarcoin{}
 			coinType = models.PTTrezarcoin
 		case coins.CCoinNameVertcoin:
+			coin = vtc.Vertcoin{}
+			coinName = vtc.Vertcoin{}
+			coinRPC = vtc.Vertcoin{}
 			coinType = models.PTVertcoin
 		default:
 			log.Fatal("Unable to determine coin choice")
@@ -139,8 +193,8 @@ var coinCmd = &cobra.Command{
 			log.Fatal("Unable to determine HomeDir")
 		}
 
-		dfRPCUser := coinRPCDefaults.RPCDefaultUsername()
-		dfRPCPort := coinRPCDefaults.RPCDefaultPort()
+		dfRPCUser := coinRPC.RPCDefaultUsername()
+		dfRPCPort := coinRPC.RPCDefaultPort()
 
 		rpcUser, rpcPassword, err = coins.PopulateConfFile(coin.ConfFile(),
 			coinHomeDir,
@@ -151,19 +205,17 @@ var coinCmd = &cobra.Command{
 
 		// ...because it's possible that the conf file for this coin has already been created, we need to store the
 		// returned user and password so, effectively, will either be storing the existing info, or
-		// the freshly generated info
+		// the freshly generated info.
 		cliConf.ProjectType = coinType
 		cliConf.RPCuser = rpcUser
 		cliConf.RPCpassword = rpcPassword
 		cliConf.ServerIP = ipAddress
+		cliConf.Port = dfRPCPort
 		if err := conf.SetConfig(cliConf); err != nil {
 			log.Fatal("Unable to write to config file: ", err)
 		}
 
 		sCoinName := coinName.CoinName()
-		if err != nil {
-			log.Fatal("Unable to GetCoinName " + err.Error())
-		}
 
 		// Now add the coin to the coin database
 		var dbCoinDetails database.CoinDetails
@@ -215,7 +267,7 @@ var coinCmd = &cobra.Command{
 			coinBC = divi.Divi{}
 		case models.PTReddCoin:
 			coinSupportsBCSnapshot = true
-			coinBC = reddcoin.ReddCoin{}
+			coinBC = rdd.ReddCoin{}
 		}
 		if coinSupportsBCSnapshot {
 			bcdExists, _ := coinBC.BlockchainDataExists()
