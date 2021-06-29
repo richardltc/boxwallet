c4ffbaed8da7859662cb2033197fb608032f6b6c
richard <richard.mace@gmail.com>
2021-06-29 07:36:11 +0100

    Refactoring.

--- cmd/cli/cmd/coins/syscoin/syscoin.go
+++ cmd/cli/cmd/coins/syscoin/syscoin.go
@@ -1,15 +1,19 @@
 package coins
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
 	"os/user"
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/coins"
+	d "richardmace.co.uk/boxwallet/cmd/cli/cmd/coins/denarius"
 	"richardmace.co.uk/boxwallet/cmd/cli/cmd/fileutils"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/mholt/archiver/v3"
@@ -49,6 +53,8 @@ const (
 	cTxFileWin     string = "syscoin-tx.exe"
 
 	cTipAddress string = "sys1qkj3tfnpndluj85k5vmfzccxfsl6nt4kn6slxey"
+
+	cWalletName string = "mainwallet"
 
 	cRPCUser string = "syscoinrpc"
 	cRPCPort string = "8370"
@@ -97,6 +103,46 @@ func (s Syscoin) AllBinaryFilesExist(dir string) (bool, error) {
 	return true, nil
 }
 
+func (s Syscoin) BlockchainInfo(auth *models.CoinAuth) (models.SYSBlockchainInfo, error) {
+	var respStruct models.SYSBlockchainInfo
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getblockchaininfo\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+auth.IPAddress+":"+auth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(auth.RPCUser, auth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func (s Syscoin) BlockchainIsSynced(coinAuth *models.CoinAuth) (bool, error) {
+	bci, err := s.BlockchainInfo(coinAuth)
+	if err != nil {
+		return false, err
+	}
+
+	if bci.Result.Verificationprogress > 0.99999 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
 func (s Syscoin) ConfFile() string {
 	return cConfFile
 }
@@ -107,6 +153,33 @@ func (s Syscoin) CoinName() string {
 
 func (s Syscoin) CoinNameAbbrev() string {
 	return cCoinNameAbbrev
+}
+
+func (s Syscoin) DaemonFilename() string {
+	if runtime.GOOS == "windows" {
+		return cDaemonFileWin
+	}
+
+	return cDaemonFileLin
+}
+
+func (s Syscoin) DaemonRunning() (bool, error) {
+	var err error
+
+	if runtime.GOOS == "windows" {
+		_, _, err = coins.FindProcess(cDaemonFileWin)
+	} else {
+		_, _, err = coins.FindProcess(cDaemonFileLin)
+	}
+
+	if err == nil {
+		return true, nil
+	}
+	if err.Error() == "not found" {
+		return false, nil
+	} else {
+		return false, err
+	}
 }
 
 // DownloadCoin - Downloads the Syscoin files into the spcified location.
@@ -143,33 +216,6 @@ func (s Syscoin) DownloadCoin(location string) error {
 		return err
 	}
 	return nil
-}
-
-func (s Syscoin) DaemonFilename() string {
-	if runtime.GOOS == "windows" {
-		return cDaemonFileWin
-	} else {
-		return cDaemonFileLin
-	}
-}
-
-func (s Syscoin) DaemonRunning() (bool, error) {
-	var err error
-
-	if runtime.GOOS == "windows" {
-		_, _, err = coins.FindProcess(cDaemonFileWin)
-	} else {
-		_, _, err = coins.FindProcess(cDaemonFileLin)
-	}
-
-	if err == nil {
-		return true, nil
-	}
-	if err.Error() == "not found" {
-		return false, nil
-	} else {
-		return false, err
-	}
 }
 
 func (s Syscoin) HomeDir() string {
@@ -263,12 +309,158 @@ func (s Syscoin) Install(location string) error {
 	return nil
 }
 
+func (s Syscoin) ListReceivedByAddress(coinAuth *models.CoinAuth, includeZero bool) (models.SYSListReceivedByAddress, error) {
+	var respStruct models.SYSListReceivedByAddress
+
+	var str string
+	if includeZero {
+		str = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listreceivedbyaddress\",\"params\":[1, true]}"
+	} else {
+		str = "{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"listreceivedbyaddress\",\"params\":[1, false]}"
+	}
+	body := strings.NewReader(s)
+	req, err := http.NewRequest("POST", "http://"+coinAuth.IPAddress+":"+coinAuth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(coinAuth.RPCUser, coinAuth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func (s Syscoin) ListTransactions(auth *models.CoinAuth) (models.SYSListTransactions, error) {
+	var respStruct models.SYSListTransactions
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandListTransactions + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+auth.IPAddress+":"+auth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(auth.RPCUser, auth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
+func (s Syscoin) NetworkDifficultyInfo() (float64, float64, error) {
+	// https://chainz.cryptoid.info/ftc/api.dws?q=getdifficulty
+
+	resp, err := http.Get("https://chainz.cryptoid.info/" + strings.ToLower(s.CoinNameAbbrev()) + "/api.dws?q=getdifficulty")
+	if err != nil {
+		return 0, 0, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	var fGood float64
+	var fWarning float64
+	// Now calculate the correct levels...
+	if fDiff, err := strconv.ParseFloat(string(body), 32); err == nil {
+		fGood = fDiff * 0.75
+		fWarning = fDiff * 0.50
+	}
+	return fGood, fWarning, nil
+}
+
+func (s Syscoin) NewAddress(auth *models.CoinAuth) (models.SYSGetNewAdddress, error) {
+	var respStruct models.SYSGetNewAdddress
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"curltext\",\"method\":\"getnewaddress\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+auth.IPAddress+":"+auth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(auth.RPCUser, auth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
+
 func (s Syscoin) RPCDefaultUsername() string {
 	return cRPCUser
 }
 
 func (s Syscoin) RPCDefaultPort() string {
 	return cRPCPort
+}
+
+func (s Syscoin) SendToAddress(coinAuth *models.CoinAuth, address string, amount float32) (returnResp models.GenericResponse, err error) {
+	var respStruct models.GenericResponse
+
+	sAmount := fmt.Sprintf("%f", amount) // sAmount == "123.456000"
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandSendToAddress + "\",\"params\":[\"" + address + "\"," + sAmount + "]}")
+	req, err := http.NewRequest("POST", "http://"+coinAuth.IPAddress+":"+coinAuth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(coinAuth.RPCUser, coinAuth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
 }
 
 func (s Syscoin) StartDaemon(displayOutput bool, appFolder string) error {
@@ -301,6 +493,13 @@ func (s Syscoin) StartDaemon(displayOutput bool, appFolder string) error {
 		}
 
 	}
+
+	// Now work out whether we need to create, or load the wallet.
+
+	// Check to see if a "mainwallet" directory exists in ~/.syscoin.
+
+	// If it does, load it, if not, create it.
+
 	return nil
 }
 
@@ -333,6 +532,87 @@ func (s Syscoin) StopDaemon(auth *models.CoinAuth) error {
 
 func (s Syscoin) TipAddress() string {
 	return cTipAddress
+}
+
+func (s Syscoin) WalletCreate(auth *models.CoinAuth) (models.SYSCreateWallet, error) {
+	var respStruct models.SYSCreateWallet
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandCreateWallet + "\",\"params\":[\"" + cWalletName + "\"]}")
+	req, err := http.NewRequest("POST", "http://"+auth.IPAddress+":"+auth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(auth.RPCUser, auth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func (s Syscoin) WalletInfo(auth *models.CoinAuth) (models.SYSWalletInfo, error) {
+	var respStruct models.SYSWalletInfo
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandGetWalletInfo + "\",\"params\":[]}")
+	req, err := http.NewRequest("POST", "http://"+auth.IPAddress+":"+auth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(auth.RPCUser, auth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
+}
+
+func (s Syscoin) WalletLoad(auth *models.CoinAuth) (models.SYSLoadWallet, error) {
+	var respStruct models.SYSLoadWallet
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandLoadWallet + "\",\"params\":[\"" + cWalletName + "\"]}")
+	req, err := http.NewRequest("POST", "http://"+auth.IPAddress+":"+auth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(auth.RPCUser, auth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+	return respStruct, nil
 }
 
 func (s *Syscoin) unarchiveFile(fullFilePath, location string) error {
