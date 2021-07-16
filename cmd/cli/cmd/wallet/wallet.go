9de5f9b6a74ba5784d7beacad0ed5369a65d55f4
richard <richard.mace@gmail.com>
2021-07-16 20:19:15 +0100

    Added getting password

--- cmd/cli/cmd/wallet/wallet.go
+++ cmd/cli/cmd/wallet/wallet.go
@@ -1,18 +1,36 @@
 package wallet
 
-import "richardmace.co.uk/boxwallet/cmd/cli/cmd/models"
+import (
+	"github.com/AlecAivazis/survey/v2"
+	be "richardmace.co.uk/boxwallet/cmd/cli/cmd/bend"
+	"richardmace.co.uk/boxwallet/cmd/cli/cmd/models"
+)
 
 type Wallet interface {
 	DaemonRunning() (bool, error)
 	WalletNeedsEncrypting(coinAuth *models.CoinAuth) (bool, error)
 	WalletLoadingStatus(coinAuth *models.CoinAuth) models.WLSType
-	WalletResync() error
+	WalletResync(appFolder string) error
+}
+
+type WalletEncrypt interface {
+	WalletEncrypt(coinAuth *models.CoinAuth, pw string) (be.GenericRespStruct, error)
 }
 
 type WalletUnlockFS interface {
-	WalletUnlockFS(coinAuth *models.CoinAuth) error
+	WalletUnlockFS(coinAuth *models.CoinAuth, password string) error
 }
 
 type WalletSecurityState interface {
 	WalletSecurityState(coinAuth *models.CoinAuth) (models.WEType, error)
+}
+
+func GetWalletEncryptionPassword() string {
+	pw := ""
+	prompt := &survey.Password{
+		Message: "Please enter your wallet password",
+	}
+	survey.AskOne(prompt, &pw)
+
+	return pw
 }
