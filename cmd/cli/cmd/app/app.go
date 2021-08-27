c6f1a30ac5b543b99f74777464fcad381bc5a5f6
richard <richard.mace@gmail.com>
2021-08-27 07:49:40 +0100

    Bumped version to 0.49.7

--- cmd/cli/cmd/app/app.go
+++ cmd/cli/cmd/app/app.go
@@ -10,7 +10,7 @@ import (
 const (
 	cAppName        string = "BoxWallet"
 	CUpdaterAppName string = "bwupdater"
-	cAppVersion     string = "0.49.6"
+	cAppVersion     string = "0.49.7"
 	CAppFilename    string = "boxwallet"
 	CAppFilenameWin string = "boxwallet.exe"
 	CAppLogfile     string = "boxwallet.log"
