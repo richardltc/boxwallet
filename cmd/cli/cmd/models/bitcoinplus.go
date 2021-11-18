6247b002bc011f88824acfd79df93dc9fa4c7854
richard <richard.mace@gmail.com>
2021-11-18 13:32:34 +0000

    Added XBCTicker struct

--- cmd/cli/cmd/models/bitcoinplus.go
+++ cmd/cli/cmd/models/bitcoinplus.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type XBCBlockchainInfo struct {
 	Result struct {
 		Chain                string  `json:"chain"`
@@ -151,6 +153,58 @@ type XBCStakingInfo struct {
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
+}
+
+type XBCTicker struct {
+	XBC struct {
+		ID                int         `json:"id"`
+		Name              string      `json:"name"`
+		Symbol            string      `json:"symbol"`
+		Slug              string      `json:"slug"`
+		NumMarketPairs    int         `json:"num_market_pairs"`
+		DateAdded         time.Time   `json:"date_added"`
+		Tags              []string    `json:"tags"`
+		MaxSupply         int         `json:"max_supply"`
+		CirculatingSupply float64     `json:"circulating_supply"`
+		TotalSupply       float64     `json:"total_supply"`
+		IsActive          int         `json:"is_active"`
+		Platform          interface{} `json:"platform"`
+		CmcRank           int         `json:"cmc_rank"`
+		IsFiat            int         `json:"is_fiat"`
+		LastUpdated       time.Time   `json:"last_updated"`
+		Quote             struct {
+			BTC struct {
+				Price                 float64   `json:"price"`
+				Volume24H             float64   `json:"volume_24h"`
+				VolumeChange24H       float64   `json:"volume_change_24h"`
+				PercentChange1H       float64   `json:"percent_change_1h"`
+				PercentChange24H      float64   `json:"percent_change_24h"`
+				PercentChange7D       float64   `json:"percent_change_7d"`
+				PercentChange30D      float64   `json:"percent_change_30d"`
+				PercentChange60D      float64   `json:"percent_change_60d"`
+				PercentChange90D      float64   `json:"percent_change_90d"`
+				MarketCap             float64   `json:"market_cap"`
+				MarketCapDominance    float64   `json:"market_cap_dominance"`
+				FullyDilutedMarketCap float64   `json:"fully_diluted_market_cap"`
+				LastUpdated           time.Time `json:"last_updated"`
+			} `json:"BTC"`
+			USD struct {
+				Price                 float64   `json:"price"`
+				Volume24H             float64   `json:"volume_24h"`
+				VolumeChange24H       float64   `json:"volume_change_24h"`
+				PercentChange1H       float64   `json:"percent_change_1h"`
+				PercentChange24H      float64   `json:"percent_change_24h"`
+				PercentChange7D       float64   `json:"percent_change_7d"`
+				PercentChange30D      float64   `json:"percent_change_30d"`
+				PercentChange60D      float64   `json:"percent_change_60d"`
+				PercentChange90D      float64   `json:"percent_change_90d"`
+				MarketCap             float64   `json:"market_cap"`
+				MarketCapDominance    float64   `json:"market_cap_dominance"`
+				FullyDilutedMarketCap float64   `json:"fully_diluted_market_cap"`
+				LastUpdated           time.Time `json:"last_updated"`
+			} `json:"USD"`
+		} `json:"quote"`
+	} `json:"XBC"`
 }
 
 type XBCWalletInfo struct {
