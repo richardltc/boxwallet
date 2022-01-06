8159a2e44defdddb7b34162c207de27ae3767636
richard <richard.mace@gmail.com>
2022-01-06 20:26:35 +0000

    Added "wallet encrypt" command for PIVX

--- cmd/cli/cmd/coins/pivx/pivx.go
+++ cmd/cli/cmd/coins/pivx/pivx.go
@@ -227,7 +227,7 @@ func (p PIVX) DaemonRunning() (bool, error) {
 	return false, err
 }
 
-// DownloadCoin - Downloads the Syscoin files into the specified location.
+// DownloadCoin - Downloads the PIVX files into the specified location.
 // "location" should just be the AppBinaryFolder ~/.boxwallet
 func (p PIVX) DownloadCoin(location string) error {
 	var fullFilePath string
@@ -970,6 +970,33 @@ func (p PIVX) WalletBackup(coinAuth *models.CoinAuth, destDir string) (models.Ge
 	return respStruct, nil
 }
 
+func (p PIVX) WalletEncrypt(coinAuth *models.CoinAuth, pw string) (models.GenericResponse, error) {
+	var respStruct models.GenericResponse
+
+	body := strings.NewReader("{\"jsonrpc\":\"1.0\",\"id\":\"boxwallet\",\"method\":\"" + models.CCommandEncryptWallet + "\",\"params\":[\"" + pw + "\"]}")
+	req, err := http.NewRequest("POST", "http://"+coinAuth.IPAddress+":"+coinAuth.Port, body)
+	if err != nil {
+		return respStruct, err
+	}
+	req.SetBasicAuth(coinAuth.RPCUser, coinAuth.RPCPassword)
+	req.Header.Set("Content-Type", "text/plain;")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return respStruct, err
+	}
+	defer resp.Body.Close()
+	bodyResp, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return respStruct, err
+	}
+	err = json.Unmarshal(bodyResp, &respStruct)
+	if err != nil {
+		return respStruct, err
+	}
+
+	return respStruct, nil
+}
 func (p PIVX) WalletInfo(auth *models.CoinAuth) (models.PIVXWalletInfo, error) {
 	var respStruct models.PIVXWalletInfo
 
