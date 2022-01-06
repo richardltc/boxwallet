5e2391377074ad215620d504145750a4c29ce628
richard <richard.mace@gmail.com>
2022-01-06 20:38:04 +0000

    Added multiple extra model structs for Trezarcoin

--- cmd/cli/cmd/models/trezarcoin.go
+++ cmd/cli/cmd/models/trezarcoin.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type TZCBlockchainInfo struct {
 	Result struct {
 		Chain                string  `json:"chain"`
@@ -43,6 +45,7 @@ type TZCBlockchainInfo struct {
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
 }
+
 type TZCGetInfo struct {
 	Result struct {
 		Version            int     `json:"version"`
@@ -71,6 +74,12 @@ type TZCGetInfo struct {
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
+}
+
+type TZCGetNewAddress struct {
+	Result string      `json:"result"`
+	Error  interface{} `json:"error"`
+	ID     string      `json:"id"`
 }
 
 type TZCListReceivedByAddress struct {
@@ -112,6 +121,30 @@ type TZCListTransactions struct {
 	ID    string      `json:"id"`
 }
 
+type TZCNetworkInfo struct {
+	Result struct {
+		Version         int    `json:"version"`
+		Subversion      string `json:"subversion"`
+		Protocolversion int    `json:"protocolversion"`
+		Localservices   string `json:"localservices"`
+		Localrelay      bool   `json:"localrelay"`
+		Timeoffset      int    `json:"timeoffset"`
+		Connections     int    `json:"connections"`
+		Networks        []struct {
+			Name                      string `json:"name"`
+			Limited                   bool   `json:"limited"`
+			Reachable                 bool   `json:"reachable"`
+			Proxy                     string `json:"proxy"`
+			ProxyRandomizeCredentials bool   `json:"proxy_randomize_credentials"`
+		} `json:"networks"`
+		Relayfee       float64       `json:"relayfee"`
+		Localaddresses []interface{} `json:"localaddresses"`
+		Warnings       string        `json:"warnings"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	Id    string      `json:"id"`
+}
+
 type TZCStakingInfo struct {
 	Result struct {
 		Enabled          bool    `json:"enabled"`
@@ -131,6 +164,58 @@ type TZCStakingInfo struct {
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
+}
+
+type TZCTicker struct {
+	TZC struct {
+		Id                int         `json:"id"`
+		Name              string      `json:"name"`
+		Symbol            string      `json:"symbol"`
+		Slug              string      `json:"slug"`
+		NumMarketPairs    int         `json:"num_market_pairs"`
+		DateAdded         time.Time   `json:"date_added"`
+		Tags              []string    `json:"tags"`
+		MaxSupply         int         `json:"max_supply"`
+		CirculatingSupply int         `json:"circulating_supply"`
+		TotalSupply       int         `json:"total_supply"`
+		IsActive          int         `json:"is_active"`
+		Platform          interface{} `json:"platform"`
+		CmcRank           int         `json:"cmc_rank"`
+		IsFiat            int         `json:"is_fiat"`
+		LastUpdated       time.Time   `json:"last_updated"`
+		Quote             struct {
+			BTC struct {
+				Price                 float64   `json:"price"`
+				Volume24H             float64   `json:"volume_24h"`
+				VolumeChange24H       float64   `json:"volume_change_24h"`
+				PercentChange1H       float64   `json:"percent_change_1h"`
+				PercentChange24H      float64   `json:"percent_change_24h"`
+				PercentChange7D       float64   `json:"percent_change_7d"`
+				PercentChange30D      float64   `json:"percent_change_30d"`
+				PercentChange60D      float64   `json:"percent_change_60d"`
+				PercentChange90D      float64   `json:"percent_change_90d"`
+				MarketCap             float64   `json:"market_cap"`
+				MarketCapDominance    int       `json:"market_cap_dominance"`
+				FullyDilutedMarketCap float64   `json:"fully_diluted_market_cap"`
+				LastUpdated           time.Time `json:"last_updated"`
+			} `json:"BTC"`
+			USD struct {
+				Price                 float64   `json:"price"`
+				Volume24H             float64   `json:"volume_24h"`
+				VolumeChange24H       float64   `json:"volume_change_24h"`
+				PercentChange1H       float64   `json:"percent_change_1h"`
+				PercentChange24H      float64   `json:"percent_change_24h"`
+				PercentChange7D       float64   `json:"percent_change_7d"`
+				PercentChange30D      float64   `json:"percent_change_30d"`
+				PercentChange60D      float64   `json:"percent_change_60d"`
+				PercentChange90D      float64   `json:"percent_change_90d"`
+				MarketCap             float64   `json:"market_cap"`
+				MarketCapDominance    int       `json:"market_cap_dominance"`
+				FullyDilutedMarketCap float64   `json:"fully_diluted_market_cap"`
+				LastUpdated           time.Time `json:"last_updated"`
+			} `json:"USD"`
+		} `json:"quote"`
+	} `json:"TZC"`
 }
 
 type TZCWalletInfo struct {
