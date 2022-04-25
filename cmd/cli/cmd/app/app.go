355a9b105f1e56a409089008232c66e756b97199
richard <richard.mace@gmail.com>
2022-04-25 07:32:28 +0100

    Upped version.

--- cmd/cli/cmd/app/app.go
+++ cmd/cli/cmd/app/app.go
@@ -10,7 +10,7 @@ import (
 const (
 	cAppName        string = "BoxWallet"
 	CUpdaterAppName string = "bwupdater"
-	CAppVersion     string = "0.51.2"
+	CAppVersion     string = "0.51.3"
 	CAppFilename    string = "boxwallet"
 	CAppFilenameWin string = "boxwallet.exe"
 	//CAppLogfile     string = "boxwallet.log"
