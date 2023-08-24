3e917fbb8e1367c6cb0039ee1a51f1360cbcb8a5
richard <richard.mace@gmail.com>
2023-08-24 08:25:05 +0100

    Removed redundant AddNodes code

--- cmd/cli/cmd/start.go
+++ cmd/cli/cmd/start.go
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -99,10 +99,10 @@ var startCmd = &cobra.Command{
 		case models.PTDivi:
 			coinDaemon = divi.Divi{}
 			coinName = divi.Divi{}
-			var d divi.Divi
-			if err := d.AddAddNodesIfRequired(); err != nil {
-				log.Fatal("Unable to add AddNodes" + err.Error())
-			}
+			//var d divi.Divi
+			//if err := d.AddAddNodesIfRequired(); err != nil {
+			//	log.Fatal("Unable to add AddNodes" + err.Error())
+			//}
 		case models.PTDogeCash:
 			coinDaemon = dogecash.DogeCash{}
 			coinName = dogecash.DogeCash{}
