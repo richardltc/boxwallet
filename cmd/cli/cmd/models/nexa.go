38aa7838e556f406649d6608e8dad888eb3e2534
richard <richard.mace@gmail.com>
2024-10-16 19:24:47 +0100

    Added NexaTicker model

--- cmd/cli/cmd/models/nexa.go
+++ cmd/cli/cmd/models/nexa.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type NEXABlockchainInfo struct {
 	Result struct {
 		Chain                string        `json:"chain"`
@@ -142,4 +144,62 @@ type NEXAWalletInfo struct {
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
+}
+
+type NEXATicker struct {
+	NEXA struct {
+		ID                            int         `json:"id"`
+		Name                          string      `json:"name"`
+		Symbol                        string      `json:"symbol"`
+		Slug                          string      `json:"slug"`
+		NumMarketPairs                int         `json:"num_market_pairs"`
+		DateAdded                     time.Time   `json:"date_added"`
+		Tags                          []string    `json:"tags"`
+		MaxSupply                     interface{} `json:"max_supply"`
+		CirculatingSupply             int64       `json:"circulating_supply"`
+		TotalSupply                   int64       `json:"total_supply"`
+		IsActive                      int         `json:"is_active"`
+		InfiniteSupply                bool        `json:"infinite_supply"`
+		Platform                      interface{} `json:"platform"`
+		CmcRank                       int         `json:"cmc_rank"`
+		IsFiat                        int         `json:"is_fiat"`
+		SelfReportedCirculatingSupply int64       `json:"self_reported_circulating_supply"`
+		SelfReportedMarketCap         float64     `json:"self_reported_market_cap"`
+		TvlRatio                      interface{} `json:"tvl_ratio"`
+		LastUpdated                   time.Time   `json:"last_updated"`
+		Quote                         struct {
+			BTC struct {
+				Price                 float64     `json:"price"`
+				Volume24H             float64     `json:"volume_24h"`
+				VolumeChange24H       float64     `json:"volume_change_24h"`
+				PercentChange1H       float64     `json:"percent_change_1h"`
+				PercentChange24H      float64     `json:"percent_change_24h"`
+				PercentChange7D       float64     `json:"percent_change_7d"`
+				PercentChange30D      float64     `json:"percent_change_30d"`
+				PercentChange60D      float64     `json:"percent_change_60d"`
+				PercentChange90D      float64     `json:"percent_change_90d"`
+				MarketCap             float64     `json:"market_cap"`
+				MarketCapDominance    int         `json:"market_cap_dominance"`
+				FullyDilutedMarketCap float64     `json:"fully_diluted_market_cap"`
+				Tvl                   interface{} `json:"tvl"`
+				LastUpdated           time.Time   `json:"last_updated"`
+			} `json:"BTC"`
+			USD struct {
+				Price                 float64     `json:"price"`
+				Volume24H             float64     `json:"volume_24h"`
+				VolumeChange24H       float64     `json:"volume_change_24h"`
+				PercentChange1H       float64     `json:"percent_change_1h"`
+				PercentChange24H      float64     `json:"percent_change_24h"`
+				PercentChange7D       float64     `json:"percent_change_7d"`
+				PercentChange30D      float64     `json:"percent_change_30d"`
+				PercentChange60D      float64     `json:"percent_change_60d"`
+				PercentChange90D      float64     `json:"percent_change_90d"`
+				MarketCap             float64     `json:"market_cap"`
+				MarketCapDominance    int         `json:"market_cap_dominance"`
+				FullyDilutedMarketCap float64     `json:"fully_diluted_market_cap"`
+				Tvl                   interface{} `json:"tvl"`
+				LastUpdated           time.Time   `json:"last_updated"`
+			} `json:"USD"`
+		} `json:"quote"`
+	} `json:"NEXA"`
 }
