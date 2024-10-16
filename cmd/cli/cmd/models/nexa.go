e0a541338ed8ab51d7fe7cadd0626d98a1d9c893
richard <richard.mace@gmail.com>
2024-10-16 19:13:49 +0100

    Added more Nexa models

--- cmd/cli/cmd/models/nexa.go
+++ cmd/cli/cmd/models/nexa.go
@@ -11,7 +11,7 @@ type NEXABlockchainInfo struct {
 		Forktime             string        `json:"forktime"`
 		Forkactive           string        `json:"forkactive"`
 		Forkactivenextblock  string        `json:"forkactivenextblock"`
-		Verificationprogress int           `json:"verificationprogress"`
+		Verificationprogress float64       `json:"verificationprogress"` // was int
 		Initialblockdownload bool          `json:"initialblockdownload"`
 		Chainwork            string        `json:"chainwork"`
 		Coinsupply           int64         `json:"coinsupply"`
@@ -52,6 +52,93 @@ type NEXAInfo struct {
 		Tokendesc       string  `json:"tokendesc"`
 		Tokenmint       string  `json:"tokenmint"`
 		Errors          string  `json:"errors"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type NEXANetworkInfo struct {
+	Result struct {
+		Version            int      `json:"version"`
+		Subversion         string   `json:"subversion"`
+		Protocolversion    int      `json:"protocolversion"`
+		Localservices      string   `json:"localservices"`
+		Localservicesnames []string `json:"localservicesnames"`
+		Timeoffset         int      `json:"timeoffset"`
+		Connections        int      `json:"connections"`
+		Networks           []struct {
+			Name                      string `json:"name"`
+			Limited                   bool   `json:"limited"`
+			Reachable                 bool   `json:"reachable"`
+			Proxy                     string `json:"proxy"`
+			ProxyRandomizeCredentials bool   `json:"proxy_randomize_credentials"`
+		} `json:"networks"`
+		Relayfee         int           `json:"relayfee"`
+		Limitfreerelay   string        `json:"limitfreerelay"`
+		Maxallowednetmsg int           `json:"maxallowednetmsg"`
+		Localaddresses   []interface{} `json:"localaddresses"`
+		Thinblockstats   struct {
+			Enabled              bool   `json:"enabled"`
+			Summary              string `json:"summary"`
+			InboundPercent       string `json:"inbound_percent"`
+			OutboundPercent      string `json:"outbound_percent"`
+			ResponseTime         string `json:"response_time"`
+			ValidationTime       string `json:"validation_time"`
+			OutboundBloomFilters string `json:"outbound_bloom_filters"`
+			InboundBloomFilters  string `json:"inbound_bloom_filters"`
+			ThinBlockSize        string `json:"thin_block_size"`
+			ThinFullTx           string `json:"thin_full_tx"`
+			Rerequested          string `json:"rerequested"`
+		} `json:"thinblockstats"`
+		Compactblockstats struct {
+			Enabled          bool   `json:"enabled"`
+			Summary          string `json:"summary"`
+			InboundPercent   string `json:"inbound_percent"`
+			OutboundPercent  string `json:"outbound_percent"`
+			ResponseTime     string `json:"response_time"`
+			ValidationTime   string `json:"validation_time"`
+			CompactBlockSize string `json:"compact_block_size"`
+			CompactFullTx    string `json:"compact_full_tx"`
+			Rerequested      string `json:"rerequested"`
+		} `json:"compactblockstats"`
+		Grapheneblockstats struct {
+			Enabled                  bool   `json:"enabled"`
+			Summary                  string `json:"summary"`
+			InboundPercent           string `json:"inbound_percent"`
+			OutboundPercent          string `json:"outbound_percent"`
+			ResponseTime             string `json:"response_time"`
+			ValidationTime           string `json:"validation_time"`
+			Filter                   string `json:"filter"`
+			Iblt                     string `json:"iblt"`
+			Rank                     string `json:"rank"`
+			GrapheneBlockSize        string `json:"graphene_block_size"`
+			GrapheneAdditionalTxSize string `json:"graphene_additional_tx_size"`
+			Rerequested              string `json:"rerequested"`
+		} `json:"grapheneblockstats"`
+		Warnings string `json:"warnings"`
+	} `json:"result"`
+	Error interface{} `json:"error"`
+	ID    string      `json:"id"`
+}
+
+type NEXAWalletInfo struct {
+	Result struct {
+		Walletversion               int     `json:"walletversion"`
+		Syncblock                   string  `json:"syncblock"`
+		Syncheight                  int     `json:"syncheight"`
+		Balance                     float64 `json:"balance"`
+		UnconfirmedBalance          float64 `json:"unconfirmed_balance"`
+		ImmatureBalance             float64 `json:"immature_balance"`
+		WatchonlyBalance            float64 `json:"watchonly_balance"`
+		UnconfirmedWatchonlyBalance float64 `json:"unconfirmed_watchonly_balance"`
+		ImmatureWatchonlyBalance    float64 `json:"immature_watchonly_balance"`
+		Txcount                     int     `json:"txcount"`
+		Unspentcount                int     `json:"unspentcount"`
+		Keypoololdest               int     `json:"keypoololdest"`
+		Keypoolsize                 int     `json:"keypoolsize"`
+		Paytxfee                    int     `json:"paytxfee"`
+		UnlockedUntil               int     `json:"unlocked_until"`
+		Hdmasterkeyid               string  `json:"hdmasterkeyid"`
 	} `json:"result"`
 	Error interface{} `json:"error"`
 	ID    string      `json:"id"`
