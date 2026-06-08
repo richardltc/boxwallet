eb0e8bbfd22d723d72563d308dfb95e067066815
Richard <richard@pop-os.lan>
2026-06-08 12:13:10 +0100

    Renamed built app to boxwallet-tui

--- zig/build.zig
+++ zig/build.zig
@@ -10,7 +10,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "boxwallet",
+        .name = "boxwallet-tui",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
